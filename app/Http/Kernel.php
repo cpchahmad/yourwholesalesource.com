ca5a8df14d4a6c0bb3d70ceb9fb85301be6af64a
Nova Solutions <fazal@novasolutions.dev>
2020-03-18 18:03:40 +0500

    17032020-lastfinal

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\SameSiteNone;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -20,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         'cors' => \App\Http\Middleware\CORS::class,
+        'samesitenone' => SameSiteNone::class,
     ];
 
     /**
