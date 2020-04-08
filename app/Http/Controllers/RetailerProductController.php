1e6e6e5c5852ed77165d00c228cddf366b5ef97a
Nova Solutions <fazal@novasolutions.dev>
2020-04-08 19:52:26 +0500

    08042020F

--- app/Http/Controllers/RetailerProductController.php
+++ app/Http/Controllers/RetailerProductController.php
@@ -389,7 +389,7 @@ class RetailerProductController extends Controller
 //            dd($product->has_platforms);
 
             $response = $shop->api()->rest('POST', '/admin/api/2019-10/products.json', $productdata);
-            dd($response);
+
             $product_shopify_id =  $response->body->product->id;
             $product->shopify_id = $product_shopify_id;
             $price = $product->price;
