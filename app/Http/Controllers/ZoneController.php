0fa02ff7c8fbea08ce5ba5c1dd3552b5efb4160c
Nova Solutions <fazal@novasolutions.dev>
2020-04-09 20:09:58 +0500

    08:09-09042020F

--- app/Http/Controllers/ZoneController.php
+++ app/Http/Controllers/ZoneController.php
@@ -75,8 +75,9 @@ class ZoneController extends Controller
             foreach ($zones as $zone) {
                 if($zone->has_rate != null){
                     if (count($zone->has_rate) > 0) {
-                        $message = '$ ' . number_format($zone->has_rate[0]->shipping_price, 2) . ' from <prp_up>HK-4</prp_up> to <prp_up>' . $country . '</prp_up> via <prp_up>' . $zone->name . ' / ' . $zone->has_rate[0]->name . '</prp_up> Ship out within 24 hours after payment,
-             Estimated delivery time ' . $zone->has_rate[0]->processing_time . '.';
+                        $message = '$ ' . number_format($zone->has_rate[0]->shipping_price, 2) . ' from <prp_up>HK-4</prp_up> to <prp_up>' . $country . '</prp_up> via <prp_up>' . $zone->name . ' / ' . $zone->has_rate[0]->name . '</prp_up> ';
+                        $message_2 = '<prp_up>'.$zone->has_rate[0]->shipping_time.'</prp_up>';
+                         $message_3 = '<prp_up>'.$zone->has_rate[0]->processing_time.'</prp_up>';
                         break;
                     }
                 }
@@ -84,9 +85,14 @@ class ZoneController extends Controller
             }
             if($message == null){
                 $message = "No Shipping Carrier Available For ".$country." Right Now!";
+                $message_2 = "Delivery Time Not Available Right Now!";
+                $message_3 = "Processing Time Not Found For This Product!";
+
                 return response()->json([
                     'status' => 'no-zone-found',
                     'message' => $message,
+                    'message_2' => $message_2,
+                    'message_3' => $message_3
                 ]);
             }
 
@@ -97,6 +103,8 @@ class ZoneController extends Controller
             return response()->json([
                 'status' => 'zones-found',
                 'message' => $message,
+                'message_2' => $message_2,
+                'message_3' => $message_3,
                 'rates' => $rates
             ]);
 
@@ -104,18 +112,26 @@ class ZoneController extends Controller
         }
         else{
             $message = "No Shipping Carrier Available For ".$country;
+            $message_2 = "Delivery Time Not Available Right Now!";
+            $message_3 = "Processing Time Not Found For This Product!";
             return response()->json([
                 'status' => 'no-zone-found',
                 'message' => $message,
+                'message_2' => $message_2,
+                'message_3' => $message_3,
             ]);
         }
 
        }
        else{
            $message = "No Shipping Carrier Available For Your ".$country;
+           $message_2 = "Delivery Time Not Available Right Now!";
+           $message_3 = "Processing Time Not Found For This Product!";
            return response()->json([
                'status' => 'no-zone-found',
                'message' => $message,
+               'message_2' => $message_2,
+               'message_3' => $message_3,
            ]);
        }
 
