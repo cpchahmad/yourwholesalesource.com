e8e0d344b2ed3ad3309502099a1544747908486e
Nova Solutions <fazal@novasolutions.dev>
2020-04-17 16:05:52 +0500

    17042020F-product_name_fix

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -4,7 +4,7 @@
         <div class="content content-full pt-2 pb-2">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill h4 my-2">
-                    {{ $product->title }}
+                    {{ \Illuminate\Support\Str::limit($product->title,20,'...') }}
                 </h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
@@ -14,7 +14,8 @@
                         <li class="breadcrumb-item" aria-current="page">
                             <a class="link-fx" href="">Products</a>
                         </li>
-                        <li class="breadcrumb-item">{{ $product->title }}</li>
+                        <li class="breadcrumb-item">  {{ \Illuminate\Support\Str::limit($product->title,20,'...') }}
+                        </li>
                     </ol>
                 </nav>
             </div>
