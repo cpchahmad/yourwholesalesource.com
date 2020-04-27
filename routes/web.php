47c863a9c54c15aa97954a78617334e472e07b13
Nova Solutions <fazal@novasolutions.dev>
2020-04-27 13:20:31 +0500

    27042020F

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', function(){
     Auth::logout();
-    session_destroy();
     return Redirect::to('login');
 })->name('logout');
 /*Super Admin Routes*/
