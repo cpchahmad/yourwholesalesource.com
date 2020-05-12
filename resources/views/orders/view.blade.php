96695f23655c635e35a8390ac4708e2a5bc7fbcf
Nova Solutions <fazal@novasolutions.dev>
2020-05-12 17:11:24 +0500

    12052020F-fix

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -87,7 +87,7 @@
                                                 @if($item->linked_real_variant != null)
                                                     <img class="img-avatar"
                                                          @if($item->linked_real_variant->has_image == null)  src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg"
-                                                         @else src="{{asset('images/variants')}}/{{$item->linked_real_variant->has_image->image}}" @endif alt="">
+                                                         @else @if($item->linked_real_variant->has_image->isV == 1) src="{{asset('images/variants')}}/{{$item->linked_real_variant->has_image->image}}" @else src="{{asset('images')}}/{{$item->linked_real_variant->has_image->image}}" @endif @endif alt="">
                                                 @else
                                                     <img class="img-avatar img-avatar-variant"
                                                          src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg">
