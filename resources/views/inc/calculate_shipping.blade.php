db1fd22b473d8f7375a811b7ad8ffa3518d195ce
Nova Solutions <fazal@novasolutions.dev>
2020-06-04 20:02:10 +0500

    0406payments

--- resources/views/inc/calculate_shipping.blade.php
+++ resources/views/inc/calculate_shipping.blade.php
@@ -16,7 +16,7 @@
         <thead>
         <tr>
             <td></td>
-            <td>Shipping Method</td>
+            <td>Rate</td>
             <td>Estimated Delivery Time</td>
             <td>Shipping Price</td>
         </tr>
@@ -25,7 +25,7 @@
         @foreach($rates as $rate)
             <tr>
                 <td><input type="radio" class="shipping_price_radio" data-country="{{$selected}}" name="shipping_price" data-price="${{number_format($rate->shipping_price,2)}}"></td>
-                <td>{{$rate->has_zone->name}} / {{$rate->name}}</td>
+                <td> {{$rate->name}}</td>
                 <td>{{$rate->shipping_time}}</td>
                 <td>${{number_format($rate->shipping_price,2)}}</td>
             </tr>
