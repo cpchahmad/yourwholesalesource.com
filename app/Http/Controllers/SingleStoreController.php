0033dd45b789f46a072566f316ddb34c9aef1bea
Nova Solutions <fazal@novasolutions.dev>
2020-07-15 13:55:03 +0500

    ip test

--- app/Http/Controllers/SingleStoreController.php
+++ app/Http/Controllers/SingleStoreController.php
@@ -167,15 +167,16 @@ class SingleStoreController extends Controller
     public function wefullfill_products(Request $request){
 
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.ipdata.co/country_name?api-key=878bb41d66f819dc08ffdec4fc14d763252af1c959f305c712443925');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            $country = 'United States';
-        }
-        curl_close($ch);
-        $country = $result;
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, 'https://api.ipdata.co/country_name?api-key=878bb41d66f819dc08ffdec4fc14d763252af1c959f305c712443925');
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        $result = curl_exec($ch);
+//        if (curl_errno($ch)) {
+//            $country = 'United States';
+//        }
+//        curl_close($ch);
+//        $country = $result;
+        $country = $this->ip_info($this->getRealIpAddr(),'Country');
 
         $categories = Category::all();
         $productQuery = Product::where('status',1)->newQuery();
@@ -562,6 +563,84 @@ class SingleStoreController extends Controller
             }
 
         }
+    }
+
+    function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+        $output = NULL;
+        if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+            $ip = $_SERVER["REMOTE_ADDR"];
+            if ($deep_detect) {
+                if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+            }
+        }
+        $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+        $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+        $continents = array(
+            "AF" => "Africa",
+            "AN" => "Antarctica",
+            "AS" => "Asia",
+            "EU" => "Europe",
+            "OC" => "Australia (Oceania)",
+            "NA" => "North America",
+            "SA" => "South America"
+        );
+        if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+            $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+            if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+                switch ($purpose) {
+                    case "location":
+                        $output = array(
+                            "city"           => @$ipdat->geoplugin_city,
+                            "state"          => @$ipdat->geoplugin_regionName,
+                            "country"        => @$ipdat->geoplugin_countryName,
+                            "country_code"   => @$ipdat->geoplugin_countryCode,
+                            "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                            "continent_code" => @$ipdat->geoplugin_continentCode
+                        );
+                        break;
+                    case "address":
+                        $address = array($ipdat->geoplugin_countryName);
+                        if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                            $address[] = $ipdat->geoplugin_regionName;
+                        if (@strlen($ipdat->geoplugin_city) >= 1)
+                            $address[] = $ipdat->geoplugin_city;
+                        $output = implode(", ", array_reverse($address));
+                        break;
+                    case "city":
+                        $output = @$ipdat->geoplugin_city;
+                        break;
+                    case "state":
+                        $output = @$ipdat->geoplugin_regionName;
+                        break;
+                    case "region":
+                        $output = @$ipdat->geoplugin_regionName;
+                        break;
+                    case "country":
+                        $output = @$ipdat->geoplugin_countryName;
+                        break;
+                    case "countrycode":
+                        $output = @$ipdat->geoplugin_countryCode;
+                        break;
+                }
+            }
+        }
+        return $output;
+    }
+    function getRealIpAddr(){
+        if ( !empty($_SERVER['HTTP_CLIENT_IP']) ) {
+            // Check IP from internet.
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+            // Check IP is passed from proxy.
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            // Get IP address from remote address.
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
 
 
