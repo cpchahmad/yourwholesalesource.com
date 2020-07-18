371e637e0634e41a1a78dab4a4b59c55255b6696
Nova Solutions <fazal@novasolutions.dev>
2020-07-18 13:06:40 +0500

    18072020F

--- resources/views/non_shopify_users/product/view_product.blade.php
+++ resources/views/non_shopify_users/product/view_product.blade.php
@@ -32,7 +32,7 @@
         <div class="block">
             <div class="block-content">
                 <div class="row items-push">
-                    <div class="col-sm-12">
+                    <div class="col-sm-6">
                         <!-- Images -->
                         <div class="row js-gallery" >
                             <?php
@@ -97,113 +97,114 @@
 
                                 <hr>
                                 @endif
-                        @if(count($product->has_subcategories) > 0)
-                            <div class="tags" style="margin-top: 5px">
-                                <h4 style="margin-bottom: 5px">Subcategories</h4>
-                                @foreach($product->has_subcategories as $subcat)
-                                    <span class="badge badge-primary">{{$subcat->title}}</span>
-                                @endforeach
+                                @if(count($product->has_subcategories) > 0)
+                                    <div class="tags" style="margin-top: 5px">
+                                        <h4 style="margin-bottom: 5px">Subcategories</h4>
+                                        @foreach($product->has_subcategories as $subcat)
+                                            <span class="badge badge-primary">{{$subcat->title}}</span>
+                                        @endforeach
+                                    </div>
+                                    <hr>
+                                @endif
+
+                                @if($product->tags != null)
+                                    <div class="tags" style="margin-top: 5px">
+
+                                        <h4 style="margin-bottom: 5px">Tags</h4>
+                                        @foreach(explode(',',$product->tags) as $tag)
+                                            <span class="badge badge-info">{{$tag}}</span>
+                                        @endforeach
+                                    </div>
+
+                            @endif
+                            <!-- END Images -->
                             </div>
-                            <hr>
-                        @endif
+                            <div class="col-sm-6">
+                                <!-- Vital Info -->
+                                <h2>
+                                    <a href="{{route('product.edit',$product->id)}}">
+                                        {{$product->title}} <span @if($product->fulfilled_by == 'AliExpress') class="badge badge-info" @else class="badge badge-primary" @endif  style="font-size: 12px;vertical-align: super">@if($product->fulfilled_by == 'AliExpress') {{$product->fulfilled_by}} @else WEFULLFILL @endif</span>
+                                    </a>
+                                </h2>
+                                <div class="clearfix" style="margin-top: 5px;width: 100%">
 
-                        @if($product->tags != null)
-                            <div class="tags" style="margin-top: 5px">
-
-                                <h4 style="margin-bottom: 5px">Tags</h4>
-                                @foreach(explode(',',$product->tags) as $tag)
-                                    <span class="badge badge-info">{{$tag}}</span>
-                                @endforeach
-                            </div>
-
-                    @endif
-                    <!-- END Images -->
-                    </div>
-                    <div class="col-sm-6">
-                        <!-- Vital Info -->
-                        <h2>
-                            <a href="{{route('product.edit',$product->id)}}">
-                                {{$product->title}} <span @if($product->fulfilled_by == 'AliExpress') class="badge badge-info" @else class="badge badge-primary" @endif  style="font-size: 12px;vertical-align: super">@if($product->fulfilled_by == 'AliExpress') {{$product->fulfilled_by}} @else WEFULLFILL @endif</span>
-                            </a>
-                        </h2>
-                        <div class="clearfix" style="margin-top: 5px;width: 100%">
-
-                            @if($product->quantity > 0)
-                                @if($product->varaint_count($product) > 0 && count($product->hasVariants) > 0)
-                                    <span class="h5">
+                                    @if($product->quantity > 0)
+                                        @if($product->varaint_count($product) > 0 && count($product->hasVariants) > 0)
+                                            <span class="h5">
                                         <span class="font-w600 text-success">IN STOCK</span><br><small>{{$product->varaint_count($product)}} Available in {{count($product->hasVariants)}} Variants</small>
                                     </span>
-                                @else
-                                    <span class="h5">
+                                        @else
+                                            <span class="h5">
                                 <span class="font-w600 text-danger">OUT OF STOCK</span><br><small>Not Available</small>
                             </span>
-                                @endif
-                            @else
-                                <span class="h5">
+                                        @endif
+                                    @else
+                                        <span class="h5">
                             <span class="font-w600 text-danger">OUT OF STOCK</span><br><small>Not Available</small>
                         </span>
-                            @endif
-                            <div class="text-right d-inline-block" style="float: right">
-                                <span class="h3 font-w700 text-success">${{number_format($product->price,2)}} </span>
-                            </div>
-                        </div>
-                        <hr>
-                        <p>{!! $product->description !!}</p>
-                        <!-- END Vital Info -->
-                    </div>
-                    <div class="col-md-12">
-                        <!-- Extra Info -->
-                        <div class="block">
-                            <ul class="nav nav-tabs nav-tabs-alt" data-toggle="tabs">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#ecom-product-comments">Varaints</a>
-                                </li>
-                            </ul>
-                            <div class="block-content tab-content">
-                                <div class="tab-pane pull-r-l active" id="ecom-product-comments">
-                                    @if(count($product->hasVariants) > 0)
-                                        <table class="table table-striped table-borderless remove-margin-b">
-                                            <thead>
-                                            <tr>
-                                                <th>Image</th>
-                                                <th>Title</th>
-                                                <th>Quantity</th>
-                                                <th>Price</th>
-                                                <th>Cost</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            @foreach($product->hasVariants as $index => $variant)
-                                                <tr>
-                                                    <td>
-                                                        <img class="img-avatar img-avatar-variant" style="border: 1px solid whitesmoke" data-form="#varaint_image_form_{{$index}}" data-input=".varaint_file_input"
-                                                             @if($variant->has_image == null)  src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg"
-                                                             @else @if($variant->has_image->isV == 1) src="{{asset('images/variants')}}/{{$variant->has_image->image}}" @else src="{{asset('images')}}/{{$variant->has_image->image}}" @endif @endif alt="">
-                                                    </td>
-
-                                                    <td>
-                                                        {{$variant->title}}
-                                                    </td>
-                                                    <td>
-                                                        @if($variant->quantity >0)
-                                                            {{$variant->quantity}}
-                                                        @else
-                                                            Out of Stock
-                                                        @endif
-                                                    </td>
-                                                    <td>${{number_format($variant->price,2)}}</td>
-                                                    <td>${{number_format($variant->cost,2)}}</td>
-                                                </tr>
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    @else
-                                        <p>This Product has Zero Variants</p>
                                     @endif
+                                    <div class="text-right d-inline-block" style="float: right">
+                                        <span class="h3 font-w700 text-success">${{number_format($product->price,2)}} </span>
+                                    </div>
                                 </div>
+                                <hr>
+                                <p>{!! $product->description !!}</p>
+                                <!-- END Vital Info -->
                             </div>
-                        </div>
-                        <!-- END Extra Info -->
+                            <div class="col-md-12">
+                                <!-- Extra Info -->
+                                <div class="block">
+                                    <ul class="nav nav-tabs nav-tabs-alt" data-toggle="tabs">
+                                        <li class="nav-item active">
+                                            <a class="nav-link" href="#ecom-product-comments">Varaints</a>
+                                        </li>
+                                    </ul>
+                                    <div class="block-content tab-content">
+                                        <div class="tab-pane pull-r-l active" id="ecom-product-comments">
+                                            @if(count($product->hasVariants) > 0)
+                                                <table class="table table-striped table-borderless remove-margin-b">
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Image</th>
+                                                        <th>Title</th>
+                                                        <th>Quantity</th>
+                                                        <th>Price</th>
+                                                        <th>Cost</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    @foreach($product->hasVariants as $index => $variant)
+                                                        <tr>
+                                                            <td>
+                                                                <img class="img-avatar img-avatar-variant" style="border: 1px solid whitesmoke" data-form="#varaint_image_form_{{$index}}" data-input=".varaint_file_input"
+                                                                     @if($variant->has_image == null)  src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg"
+                                                                     @else @if($variant->has_image->isV == 1) src="{{asset('images/variants')}}/{{$variant->has_image->image}}" @else src="{{asset('images')}}/{{$variant->has_image->image}}" @endif @endif alt="">
+                                                            </td>
+
+                                                            <td>
+                                                                {{$variant->title}}
+                                                            </td>
+                                                            <td>
+                                                                @if($variant->quantity >0)
+                                                                    {{$variant->quantity}}
+                                                                @else
+                                                                    Out of Stock
+                                                                @endif
+                                                            </td>
+                                                            <td>${{number_format($variant->price,2)}}</td>
+                                                            <td>${{number_format($variant->cost,2)}}</td>
+                                                        </tr>
+                                                    @endforeach
+                                                    </tbody>
+                                                </table>
+                                            @else
+                                                <p>This Product has Zero Variants</p>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- END Extra Info -->
+                            </div>
                     </div>
                 </div>
             </div>
