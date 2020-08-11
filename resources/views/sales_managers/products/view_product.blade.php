9ca0b19a79517fae0f83aed5ae9c1f898b441c46
Nova Solutions <fazal@novasolutions.dev>
2020-08-11 16:11:33 +0500

    11082020F

--- resources/views/sales_managers/products/view_product.blade.php
+++ resources/views/sales_managers/products/view_product.blade.php
@@ -143,7 +143,7 @@
                                     <span class="h5">
                                         <span class="font-w600 text-success">IN STOCK</span><br><small>{{$product->varaint_count($product)}} Available in {{count($product->hasVariants)}} Variants</small>
                                     </span>
-                                @elseif(count($product->quantity) > 0)
+                                @elseif($product->quantity > 0)
                                     <span class="h5">
                                         <span class="font-w600 text-success">IN STOCK</span><br><small>{{$product->quantity}} Available  </small>
                                     </span>
