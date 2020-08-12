e543d958e2d7a71c5b88d1182d45aea95fd86ceb
Nova Solutions <fazal@novasolutions.dev>
2020-08-12 18:57:53 +0500

    12082020F-fix

--- resources/views/layout/shopify.blade.php
+++ resources/views/layout/shopify.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html>
-    @include('inc.header')
+@include('inc.header')
 <body>
 <div id="page-container" class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed">
     @include('layout.shopify_sidebar')
     <main id="main-container">
         @include('flash_message.message')
+        @if(auth()->check())
+            @if(count(auth()->user()->has_shops) == 0)
+                <div class="alert alert-info alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>You dont have connected any store yet. For store connection <a href="{{route('system.store.connect')}}">click here</a>.</strong>
+                </div>w
+            @endif
+        @endif
         @yield('content')
     </main>
 
-        @include('inc.footer')
+    @include('inc.footer')
 </div>
 
 </body>
