efc0380d6ebb48d88100a68f47af68c49ec82bde
Nova Solutions <fazal@novasolutions.dev>
2020-08-12 13:43:00 +0500

    12082020F

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -78,7 +78,7 @@ class InventoryController extends Controller
 
         $products = Product::whereNotNull('shopify_id')->get();
         foreach ($products as $product){
-            $response =   $shop->api()->rest('GET', '/admin/api/2019-10/products/'. $product->shopify_id .'.json');
+                        $response =   $shop->api()->rest('GET', '/admin/api/2019-10/products/'. $product->shopify_id .'.json');
             if(!$response->errors){
                 $shopifyVariants = $response->body->product->variants;
                 if(count($product->hasVariants) == 0){
@@ -98,6 +98,7 @@ class InventoryController extends Controller
             else{
                 dd($response);
             }
+            sleep(3);
 
 
         }
@@ -133,5 +134,6 @@ class InventoryController extends Controller
         ];
 
         $res = $shop->api()->rest('POST', '/admin/api/2020-07/inventory_levels/set.json', $data);
+        dd($res);
     }
 }
