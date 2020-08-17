9fac15259d7ff9ef11e898517e7d2fb97c6c5002
Nova Solutions <fazal@novasolutions.dev>
2020-08-17 19:04:36 +0500

    fix-variant-page

--- resources/views/layout/single.blade.php
+++ resources/views/layout/single.blade.php
@@ -71,7 +71,7 @@
         <div class="content py-3">
             <div class="row font-size-sm">
                 <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-right">
-                    Designed by <i class="fa fa-bolt text-danger"></i> by <a class="font-w600" href="https://tetralogicx.com" target="_blank">Fantasy Supply Limited</a>
+                    Designed <i class="fa fa-bolt text-danger"></i> by <a class="font-w600" href="https://tetralogicx.com" target="_blank">Fantasy Supply Limited</a>
                 </div>
             </div>
         </div>
