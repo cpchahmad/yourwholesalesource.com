ecc3ea905b6842c53d24151eb2c80c49ee586407
Nova Solutions <fazal@novasolutions.dev>
2020-08-20 15:50:26 +0500

    custom-order-fix-20082020F

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -568,10 +568,7 @@ class AdminOrderController extends Controller
             ->take(10);
 
         $top_users = User::role('non-shopify-users')->join('retailer_orders', function ($o) {
-            $o->on('retailer_orders.user_id', '=', 'users.id')
-                ->join('retailer_order_line_items',function($j){
-                    $j->on('retailer_order_line_items.retailer_order_id', '=', 'retailer_orders.id');
-                });
+            $o->on('retailer_orders.user_id', '=', 'users.id');
         }) ->where('retailer_orders.paid','>=',1)
             ->where('retailer_orders.custom','=',1)
             ->select('users.*', DB::raw('COUNT(retailer_orders.cost_to_pay) as sold'), DB::raw('sum(retailer_orders.cost_to_pay) as selling_cost'))
