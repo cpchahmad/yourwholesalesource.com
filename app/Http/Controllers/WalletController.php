b1e0c92e32a60b2faa1575d1d17499b158e90d9a
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-16 16:29:02 +0500

    Implementing Wallet request page

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -172,10 +172,13 @@ class WalletController extends Controller
     }
 
     public function walletRequest(Request $request){
+
+        $wallets = Wallet::requests()->where('status', 0)->get();
+        dd($wallets);
         $wallets = [];
 
         foreach (Wallet::all() as $wallet) {
-            if($wallet->request()->where('status', 0)->count() > 0) {
+            if($wallet->requests()->where('status', 0)->count() > 0) {
                 array_push($wallets, $wallet);
             }
         }
