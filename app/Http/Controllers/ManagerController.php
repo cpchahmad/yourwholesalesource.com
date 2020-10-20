64c96ad340284fd75818c94af21afb0a8342ecfb
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-20 18:04:28 +0500

    Fixing issues related to combining shopify and non-shopify users

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -798,7 +798,7 @@ class ManagerController extends Controller
 
 
         if($request->has('user_search')){
-            $x = User::where('sale_manager_id', Auth::id)->get();
+            $x = User::where('sale_manager_id', Auth::id())->get();
             dd($x);
             $u = User::where('sale_manager_id', Auth::id())->where('name','LIKE','%'.$request->input('user_search').'%')->orWhere('email','LIKE','%'.$request->input('user_search').'%')->get();
 
