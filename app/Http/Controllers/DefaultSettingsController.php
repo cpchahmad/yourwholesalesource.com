769b1c57f291e5c9c58989891875d0b38fa468f0
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-20 16:22:53 +0500

    Fixing issues related to combining shopify and non-shopify users

--- app/Http/Controllers/DefaultSettingsController.php
+++ app/Http/Controllers/DefaultSettingsController.php
@@ -445,7 +445,7 @@ class DefaultSettingsController extends Controller
 
         $users->whereNotIn('email', ['admin@wefullfill.com', 'super_admin@wefullfill.com']);
         if($request->has('user_search')){
-            $users = $stores->where('shopify_domain','LIKE','%'.$request->input('user_search').'%')->shop_user()->user_id;
+            $users = $stores->where('shopify_domain','LIKE','%'.$request->input('user_search').'%')->has_user()->user_id;
             dd($users);
             $users->orWhere('name','LIKE','%'.$request->input('user_search').'%');
             $users->orWhere('email','LIKE','%'.$request->input('user_search').'%');
