dc1d71826bb43957aa27620670f76bd0378454a8
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-22 16:28:32 +0500

    Enhancing Variant update functionality

--- app/Traits/ProductVariantTrait.php
+++ app/Traits/ProductVariantTrait.php
@@ -6,7 +6,7 @@ trait ProductVariantTrait {
 
     public function option1($product){
         if(count($product->hasVariants) > 0){
-            $product->hasVariants()->refresh();
+            $product->hasVariants()->fresh();
             $array = array_unique($product->hasVariants->pluck('option1')->toArray());
             if($array[0] == ""){
                 $array =[];
@@ -20,7 +20,7 @@ trait ProductVariantTrait {
     }
     public function option2($product){
         if(count($product->hasVariants) > 0){
-            $product->hasVariants()->refresh();
+            $product->hasVariants()->fresh();
 
             $array =  array_unique($product->hasVariants->pluck('option2')->toArray());
             if($array[0] == ""){
@@ -34,7 +34,7 @@ trait ProductVariantTrait {
     }
     public function option3($product){
         if(count($product->hasVariants) > 0){
-            $product->hasVariants()->refresh();
+            $product->hasVariants()->fresh();
 
             $array =  array_unique($product->hasVariants->pluck('option3')->toArray());
             if($array[0] == ""){
