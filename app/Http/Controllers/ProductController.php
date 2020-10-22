e58a947425179257cc5d01d920e9830f990becce
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-22 16:37:48 +0500

    Enhancing Variant update functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -225,7 +225,6 @@ class ProductController extends Controller
                         ]
                     ];
 
-                    dd($productdata);
 
                     $resp =  $shop->api()->rest('PUT', '/admin/api/2019-10/products/'.$product->shopify_id.'.json',$productdata);
                     $shopifyVariants = $resp->body->product->variants;
