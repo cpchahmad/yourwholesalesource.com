82b1add4cd963fba369cff74b76d4dbb3a94b7e0
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-22 17:52:54 +0500

    Enhancing Variant update functionality

---
+++ app/Http/Middleware/CheckShopUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use OhMyBrew\ShopifyApp\Facades\ShopifyApp;
+
+class CheckShopUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        if(ShopifyApp::shop() != null){
+            return $next($request);
+        }
+        else{
+            return redirect()->route('store.index');
+        }
+    }
+}
