67a084cf1b6cbdcc3a04e263b2ba90baf873dd04
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-23 14:38:09 +0500

    Email Template

--- app/Mail/OrderPlaceEmail.php
+++ app/Mail/OrderPlaceEmail.php
@@ -8,6 +8,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
 
 class OrderPlaceEmail extends Mailable
 {
