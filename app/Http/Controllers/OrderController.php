1b135da36e6040e13c0c0e7cc9740fa8f9602c60
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-23 15:21:53 +0500

    Email Template

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -105,6 +105,7 @@ class OrderController extends Controller
             if($user->has_manager()->count() > 0) {
                 $manager_email = $user->has_manager->email;
             }
+
             $manager_email = $user->has_manager->email;
             $users_temp =['info@wefullfill.com',$manager_email];
             $users = [];
