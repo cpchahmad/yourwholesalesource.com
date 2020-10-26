a463f74adf6c8389d45c3f84c965192389bec038
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-26 13:16:24 +0500

    Edit Email template functionality

--- app/Http/Controllers/EmailTemplateController.php
+++ app/Http/Controllers/EmailTemplateController.php
@@ -52,34 +52,7 @@ class EmailTemplateController extends Controller
      */
     public function show($id)
     {
-//        switch ($id) {
-//            case "1":
-//                return view('emails.new_user')->with('user', User::find(1));
-//                break;
-//            case "2":
-//                return view('emails.new_shopify_user')->with('user', User::find(1));
-//                break;
-//            case "3":
-//                return view('emails.order_place')->with('user', User::find(1))->with('retail_order', RetailerOrder::find(1));
-//                break;
-//            case "4":
-//                return view('emails.order_status')->with('user', User::find(1))->with('order', RetailerOrder::find(1));
-//                break;
-//            case "5":
-//                return view('emails.wishlist_request')->with('wishlist', Wishlist::find(1));
-//                break;
-//            case "6":
-//                return view('emails.wallet_reqeust')->with('wallet', Wallet::find(1));
-//                break;
-//            case "7":
-//                return view('emails.refund_request')->with('ticket', Ticket::find(1));
-//                break;
-//            default:
-//                echo "Error";
-//        }
-
         return view('setttings.email.show')->with('template', EmailTemplate::find($id));
-
     }
 
     /**
@@ -90,32 +63,7 @@ class EmailTemplateController extends Controller
      */
     public function edit($id)
     {
-        switch ($id) {
-            case "1":
-                $template = EmailTemplate::find(1);
-                return view('emails.new_user')->with('template', $template)->with('edit', 1);
-                break;
-            case "2":
-                return view('emails.new_shopify_user')->with('user', User::find(1))->with('edit', 1);
-                break;
-            case "3":
-                return view('emails.order_place')->with('user', User::find(1))->with('retail_order', RetailerOrder::find(1))->with('edit', 1);
-                break;
-            case "4":
-                return view('emails.order_status')->with('user', User::find(1))->with('order', RetailerOrder::find(1))->with('edit', 1);
-                break;
-            case "5":
-                return view('emails.wishlist_request')->with('wishlist', Wishlist::find(1))->with('edit', 1);
-                break;
-            case "6":
-                return view('emails.wallet_reqeust')->with('wallet', Wallet::find(1))->with('edit', 1);
-                break;
-            case "7":
-                return view('emails.refund_request')->with('ticket', Ticket::find(1))->with('edit', 1);
-                break;
-            default:
-                echo "Error";
-        }
+        return view('setttings.email.show')->with('template', EmailTemplate::find($id))->with('email', 1);
     }
 
     /**
--- resources/views/setttings/email/show.blade.php
+++ resources/views/setttings/email/show.blade.php
@@ -82,7 +82,7 @@
                             @else
                                 <div class="email-content-detail" style="margin: 50px 0;">
                                     <h1 class="email-title" style="margin: 0;margin-bottom: 30px;font-size: 34px;">{{ $template->subject }}</h1>
-                                    <p class="email-message-1" style=" margin: 0;margin-bottom: 30px;font-size: 20px;line-height: 1.53;" >{{ $template->body }}. </p>
+                                    <p class="email-message-1" style=" margin: 0;margin-bottom: 30px;font-size: 20px;line-height: 1.53;" >{{ $template->body }} </p>
 
                                     <a href="https://www.wefullfill.com/pages/help-center" target="_blank" class="email_btn" style="padding: 17px 55px; border: 2px solid #7daa40;font-size: 20px;letter-spacing: 1px;text-decoration: none;color: #7daa40;margin-top: 0;FONT-WEIGHT: 600;margin-bottom: 25px;margin-top: 25px">Help Center</a>
                                 </div>
