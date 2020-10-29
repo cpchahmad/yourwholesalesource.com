8a4fae8f2a5bb54de0c6dadf9134580b46622a7f
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-10-29 17:38:31 +0500

    Resolving Product Images shuffling issue

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -66,7 +66,7 @@
 
                                     @endphp
                                     @foreach(DB::table('images')->where('product_id', $product->id)->orderByRaw("CAST(position as UNSIGNED) ASC")->get() as $image)
-                                        <div class="col-lg-4 preview-image animated fadeIn" data-id="{{$image->id}}" data-pos="{{$image->position}}">
+                                        <div class="col-lg-4 preview-image animated fadeIn" data-id="{{$image->id}}">
                                             <div class="options-container fx-img-zoom-in fx-opt-slide-right">
                                                 @if($image->isV == 0)
                                                     <img class="img-fluid options-item" src="{{asset('images')}}/{{$image->image}}" alt="" >
