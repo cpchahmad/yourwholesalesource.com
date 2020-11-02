3690ab332c4e380ea38c75377da39350808cb2c1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-02 18:03:55 +0500

    Admin Sync store orders

--- resources/views/single-store/wishlist/view.blade.php
+++ resources/views/single-store/wishlist/view.blade.php
@@ -213,6 +213,10 @@
                                         <div class="push-20">
                                             <a class="btn btn-sm btn-primary" href="{{route('store.product.wefulfill.show',$wishlist->has_product->id)}}">View</a>
 
+                                            @php
+                                                dd(23);
+                                            @endphp
+
                                             @if(!in_array($wishlist->has_product->id,$wishlist->has_store->has_imported->pluck('id')->toArray()))
                                                 <a class="btn btn-sm btn-success" href="{{route('store.product.wefulfill.add-to-import-list',$wishlist->has_product->id)}}">
                                                     <i class="fa fa-plus"></i> Add to Import List
@@ -234,9 +238,7 @@
 
                                 @endif
                                 <hr>
-                                @php
-                                    dd($wishlist->has_store->has_imported->pluck('id')->toArray())
-                                @endphp
+
 
                                 @if(!in_array($wishlist->has_product->id,$wishlist->has_store->has_imported->pluck('id')->toArray()))
                                     <button onclick="window.location.href='{{ route('store.product.wefulfill.add-to-import-list',$wishlist->has_product->id)}}'" class="btn btn-primary btn-block mb2"><i class="fa fa-plus"></i> Add to Import List</button>
