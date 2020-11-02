f41dc8276c87f3cf0fb4653c25dedbbeedf5fce0
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-02 13:32:17 +0500

    Testing old stores webhooks

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -56,7 +56,7 @@ class AdminOrderController extends Controller
 
         $response = $shop->api()->rest('GET', '/admin/webhooks.json');
 
-        dd(55, $response);
+        dd(45, $response);
 
     }
 
