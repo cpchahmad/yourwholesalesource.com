808cc35741db3c639233db707bd5108a62d4f9c0
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-04 16:05:09 +0500

    Adding Reports to shopify users

--- resources/views/single-store/reports.blade.php
+++ resources/views/single-store/reports.blade.php
@@ -220,9 +220,14 @@
                 const contentDataURL = canvas.toDataURL('image/png')
                 let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
                 var position = 0;
-                pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+                pdf.addImage(contentDataURL, 'JPEG', 0, position, imgWidth, imgHeight);
                 //  pdf.save('new-file.pdf');
                 window.open(pdf.output('bloburl', { filename: 'new-file.pdf' }), '_blank');
+
+                // var img = canvas.toDataURL('image/png');
+                // var doc = new jsPDF();
+                // doc.addImage(img, 'JPEG', 20, 20);
+                // doc.save('new-file.pdf');
             });
 
         });
