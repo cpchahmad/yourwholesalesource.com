f1bb630482463d91d3c742e16425ebdd2b24e31a
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-04 15:21:32 +0500

    Adding Reports to shopify users

--- resources/views/single-store/reports.blade.php
+++ resources/views/single-store/reports.blade.php
@@ -169,7 +169,7 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.1.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
     <script>
         if($('body').find('#reportrange').length > 0){
