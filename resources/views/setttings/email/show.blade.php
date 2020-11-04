19599fd4c7512d7b93a57cb5998a4cd328862e10
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-04 18:43:26 +0500

    Adding Product recommendation email

--- resources/views/setttings/email/show.blade.php
+++ resources/views/setttings/email/show.blade.php
@@ -285,7 +285,14 @@
                                             </div>
                                             <select class="@error('type') is-invalid @enderror js-select2 form-control" name="products[]" style="width: 100%; border-radius: 0 !important;" data-placeholder="Select Products.." multiple>
                                                @foreach($products as $product)
-                                                    <option value="{{ $product->id }}">{{ $product->title }}</option>
+                                                    <option value="{{ $product->id }}"
+                                                        @php
+                                                            $prods = json_decode($template->products);
+                                                            if(in_array($product->id, $prods)){
+                                                                echo "selected";
+                                                            }
+                                                        @endphp
+                                                    >{{ $product->title }}</option>
                                                @endforeach
 
                                             </select>
