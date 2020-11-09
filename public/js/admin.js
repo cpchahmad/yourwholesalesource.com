6d0790fbb24e03d45d5b34348ccabc610c00bff8
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-09 14:06:31 +0500

    Working on tiered pricing for product without variants

--- public/js/admin.js
+++ public/js/admin.js
@@ -72,7 +72,7 @@ $(document).ready(function () {
                 var response = res.data;
                 if(response == 'success') {
                     console.log(row);
-                    row.hide();
+                    row.empty();
                 }
             }
         });
