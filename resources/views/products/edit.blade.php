4540f686508603cae47111702038204cdb58bcfe
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-09 13:01:56 +0500

    Working on tiered pricing for product

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -749,7 +749,7 @@
                                                                         <input  type="number" step="any" class="form-control" name="tiered_price{{$v->id}}[]"  value="{{ number_format($item->price, 2) }}" placeholder="$0.0">
                                                                     </div>
                                                                     <div class="col-md-2 btn-group btn-group-sm" role="group">
-                                                                        <button type="button" class="btn btn-sm btn-danger remove-price-row-from-db-btn" data-item="{{ $item->id }}">-</button>
+                                                                        <button type="button" class="btn btn-sm btn-danger remove-price-row-from-db-btn" style="font-size: 11px; !important;" data-item="{{ $item->id }}">Remove</button>
                                                                     </div>
                                                                 </div>
                                                             @endforeach
