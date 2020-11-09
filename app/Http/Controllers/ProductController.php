700016f38fe28cdeae83beae010999d0d67689da
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-09 18:25:34 +0500

    Calculating tiered pricing in order line item and performing calculations

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -106,10 +106,8 @@ class ProductController extends Controller
                 TieredPrice::where('product_variant_id', $variant)->where('product_id', $id)->delete();
             }
 
-
-            foreach($request->input('min_qty'.$variant) as $i => $row) {
-
-                dump($i);
+            dump(count($request->input('min_qty'.$variant)));
+            for($i=0; $i< count($request->input('min_qty'.$variant)); $i++) {
 
                 if($request->input('min_qty'.$variant)[$i] != null) {
                     $item = new TieredPrice();
