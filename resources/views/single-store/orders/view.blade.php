0257fb967e9628d8ccf1040ede83e5503dfa496c
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-09 19:25:23 +0500

    Calculating tiered pricing in order line item and performing calculations

--- resources/views/single-store/orders/view.blade.php
+++ resources/views/single-store/orders/view.blade.php
@@ -255,6 +255,8 @@
                                                            $price = null;
                                                            if($item->quantity >= $var_price->min_qty && $item->qunatity <= $var_price->max_qty) {
                                                                echo $var_price->min_qty;
+                                                               echo $var_price->max_qty;
+                                                               echo $item->quantity;
                                                                if($var_price->type == 'fixed') {
                                                                    $price = $var_price->price;
                                                                    $price = number_format($price, 2);
