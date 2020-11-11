2c22236504f1caeecd1b2f9f3abb13287ac65baa
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-11 17:10:25 +0500

    Adding General Discount Preferences Feature

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -138,20 +138,20 @@
 
             <li class="nav-main-item">
                 <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
-                    <i class="nav-main-link-icon si-basket-loaded"></i>
+                    <i class="nav-main-link-icon si si-basket-loaded"></i>
                     <span class="nav-main-link-name">Discounts</span>
                 </a>
                 <ul class="nav-main-submenu">
                     <li class="nav-main-item">
                         <a class="nav-main-link" href="{{ route('admin.tiered.pricing.preferences')}}">
-                            <i class="nav-main-link-icon si-basket-loaded"></i>
+                            <i class="nav-main-link-icon si si-basket-loaded"></i>
                             <span class="nav-main-link-name">Tiered Pricing</span>
                         </a>
                     </li>
 
                     <li class="nav-main-item">
                         <a class="nav-main-link" href="{{ route('admin.general.discount.preferences')}}">
-                            <i class="nav-main-link-icon si-basket-loaded"></i>
+                            <i class="nav-main-link-icon si si-basket-loaded"></i>
                             <span class="nav-main-link-name">General Discounts</span>
                         </a>
                     </li>
