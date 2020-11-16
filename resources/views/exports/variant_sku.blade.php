3c8c464c672cebe1d54869ee3b2f5089e136f151
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-16 19:39:31 +0500

    Adding Download SKU list feature

--- resources/views/exports/variant_sku.blade.php
+++ resources/views/exports/variant_sku.blade.php
@@ -2,14 +2,28 @@
     <thead>
     <tr>
         <th>SKU</th>
+        <th>Option</th>
+        <th>Image Url</th>
     </tr>
     </thead>
 
 
     <tbody class="">
-    @foreach($variants as $variant)
+    @foreach($variants as $v)
         <tr>
-            <td class="font-w600">{{ $variant->sku }}</td>
+            <td class="font-w600">{{ $v->sku }}</td>
+            <td class="font-w600">@if($v->option1 != null) {{$v->option1}} @endif    @if($v->option2 != null) / {{$v->option2}} @endif    @if($v->option3 != null) / {{$v->option3}} @endif</td>
+            <td class="font-w600">
+                @if($v->has_image == null)
+                    No image Found
+                @else
+                    @if($v->has_image->isV == 0)
+                        {{asset('images')}}/{{$v->has_image->image}}
+                    @else
+                        {{asset('images/variants')}}/{{$v->has_image->image}}
+                    @endif
+                @endif
+            </td>
         </tr>
     @endforeach
     </tbody>
