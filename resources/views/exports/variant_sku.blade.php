e44975a382a53e6118b197ad40292461b3e2b129
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-17 14:58:21 +0500

    Adding image url to sku csv

--- resources/views/exports/variant_sku.blade.php
+++ resources/views/exports/variant_sku.blade.php
@@ -13,7 +13,19 @@
         <tr>
             <td class="font-w600">{{ $v->sku }}</td>
             <td class="font-w600">@if($v->option1 != null) {{$v->option1}} @endif    @if($v->option2 != null) / {{$v->option2}} @endif    @if($v->option3 != null) / {{$v->option3}} @endif</td>
-            <td>Still to implement</td>
+            <td>
+                @php
+                    if($v->has_image == null)
+                        $img = 'No Image Found';
+                    else {
+                        if($v->has_image->isV == 0)
+                            $img="asset('images')}}/$v->has_image->image";
+                        else
+                            $img="asset('images/variants')/$v->has_image->image";
+                    }
+                @endphp
+                {{ $img }}
+            </td>
         </tr>
     @endforeach
     </tbody>
