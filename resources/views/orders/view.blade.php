e6dbe83148618916d04637d7be37191518e40649
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-18 12:16:03 +0500

    Fixing General Fixed discount functionality

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -270,7 +270,7 @@
                                             @endif
 
                                             @if($is_general_discount && $is_applied_for_general_fixed)
-                                                {{ number_format(\App\GeneralFixedPricePreferences::first()->fixed_amount * ($line_items->sum(quantity) - 1), 2) }} $ off on whole order
+                                                {{ number_format(\App\GeneralFixedPricePreferences::first()->fixed_amount * ($order->line_items->sum(quantity) - 1), 2) }} $ off on whole order
 
                                             @endif
 
