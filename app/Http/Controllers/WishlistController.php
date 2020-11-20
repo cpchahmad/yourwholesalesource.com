f1dbc4021436f0f7e25f6eb352801b5d113e2dd1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-20 12:43:44 +0500

    Fixing Wishlist push issue

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -684,9 +684,9 @@ class WishlistController extends Controller
         $options_array = [];
         $images_array = [];
         //converting variants into shopify api format
-        $variants_array = $this->variants_template_array($prod, $variants_array);
+        $variants_array = $this->variants_template_array($product, $variants_array);
         /*Product Options*/
-        $options_array = $this->options_template_array($prod, $options_array);
+        $options_array = $this->options_template_array($product, $options_array);
         /*Product Images*/
 
 
