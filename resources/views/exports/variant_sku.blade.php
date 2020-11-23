1de5eb21305d6f4a40cf988983fc883d25f47e56
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 14:19:21 +0500

    Fixing SKU csv bug

--- resources/views/exports/variant_sku.blade.php
+++ resources/views/exports/variant_sku.blade.php
@@ -21,11 +21,11 @@
                     }
                     else {
                         if($v->has_image->isV == 0) {
-                            $img= (string) asset('images') .'/'. $v->has_image->image;
+                            $img= (string) asset('images') . $v->has_image->image;
                             echo $img;
                         }
                         else {
-                            $img= (string) asset('images/variants') .'/'. $v->has_image->image;
+                            $img= (string) asset('images/variants') . $v->has_image->image;
                             echo $img;
                         }
 
