09772ed108178d10bab2d9e98211e9b038d9be89
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 14:06:08 +0500

    Fixing SKU csv bug

--- resources/views/exports/variant_sku.blade.php
+++ resources/views/exports/variant_sku.blade.php
@@ -19,7 +19,7 @@
                         $img = 'No Image Found';
                     else {
                         if($v->has_image->isV == 0)
-                            $img= asset('images') .'/'. $v->has_image->image;
+                            $img= asset('images').'/'.$v->has_image->image;
                         else
                             $img= asset('images/variants').'/'.$v->has_image->image;
                     }
