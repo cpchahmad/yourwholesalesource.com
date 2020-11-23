d48c12a90917e9a5b82a3286145dd7a7f01d1340
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 15:06:44 +0500

    Fixing Admin sync order button

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -209,6 +209,7 @@ class OrderController extends Controller
                                     $customer->total_spent = $order->customer->total_spent;
                                     $customer->shop_id = $shop->id;
                                     $local_shop = $shop;
+                                    dd($local_shop);
                                     if (count($local_shop->has_user) > 0) {
                                         $customer->user_id = $local_shop->has_user[0]->id;
                                     }
@@ -227,7 +228,7 @@ class OrderController extends Controller
                             $new->status = 'new';
                             $new->shop_id = $shop->id;
                             $local_shop = $shop;
-                            dd($local_shop);
+
                             if (count($local_shop->has_user) > 0) {
                                 $new->user_id = $local_shop->has_user[0]->id;
                             }
