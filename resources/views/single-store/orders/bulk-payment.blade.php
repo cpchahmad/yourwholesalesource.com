5ad407d867e9e287186fc9396566284f20d573e1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 16:18:53 +0500

    Adding discount section on bulk payment

--- resources/views/single-store/orders/bulk-payment.blade.php
+++ resources/views/single-store/orders/bulk-payment.blade.php
@@ -20,6 +20,7 @@
                                     <th style="width: 10%">Name</th>
                                     <th>Fulfilled By</th>
                                     <th>Cost</th>
+                                    <th>Discount</th>
                                     <th>Price X Quantity</th>
                                     <th>Status</th>
                                     <th>Billing Address</th>
@@ -27,6 +28,46 @@
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @php
+                                    $total_discount = 0;
+                                    $n = $order->line_items->where('fulfilled_by', '!=', 'store')->sum('quantity');
+                                    $line_item_count = count($order->line_items);
+
+                                    if($order->line_items->where('fulfilled_by', '!=', 'store')->count() >=2){
+                                        $is_general_discount = true;
+                                    }
+                                    else {
+                                        $is_general_discount = false;
+                                    }
+
+                                    if(\App\GeneralDiscountPreferences::first()->global == 1) {
+                                        $is_applied_for_general_dsiscount = true;
+                                    }
+                                    else {
+                                        $stores = \App\GeneralDiscountPreferences::first()->stores_id;
+                                        $store_array= json_decode($stores);
+                                        if(in_array($shop->id, $store_array)) { $is_applied_for_general_dsiscount = true; } else { $is_applied_for_general_dsiscount = false; }
+                                    }
+
+                                    if(\App\GeneralFixedPricePreferences::first()->global == 1) {
+                                        $is_applied_for_general_fixed = true;
+                                    }
+                                    else {
+                                        $stores = \App\GeneralFixedPricePreferences::first()->stores_id;
+                                        $store_array= json_decode($stores);
+                                        if(in_array($shop->id, $store_array)) { $is_applied_for_general_fixed = true; } else { $is_applied_for_general_fixed = false; }
+                                    }
+
+                                    if(\App\TieredPricingPrefrences::first()->global == 1) {
+                                        $is_applied = true;
+                                    }
+                                    else {
+                                        $stores = \App\TieredPricingPrefrences::first()->stores_id;
+                                        $store_array= json_decode($stores);
+                                        if(in_array($shop->id, $store_array)) { $is_applied = true; } else { $is_applied = false; }
+                                    }
+
+                                @endphp
                                 @foreach($order->line_items as $item)
                                     @if($item->fulfilled_by != 'store')
                                         <tr>
@@ -96,6 +137,65 @@
                                             </td>
 
                                             <td>{{number_format($item->cost,2)}}  X {{$item->quantity}}  USD</td>
+                                            <td>
+                                                @php
+                                                    $variant = $item->linked_variant;
+                                                    $real_variant = null;
+
+
+                                                    if($variant) {
+                                                        $real_variant = \App\ProductVariant::where('sku', $variant->sku)->first();
+                                                    }
+                                                    else{
+                                                        $retailer_product = $item->linked_product;
+                                                        $real_variant = \App\Product::where('title', $retailer_product->title)->first();
+                                                    }
+                                                @endphp
+                                                @if($real_variant != null && $is_applied && !($is_general_discount))
+                                                    @if(count($real_variant->has_tiered_prices) > 0)
+                                                        @foreach($real_variant->has_tiered_prices as $var_price)
+                                                            @php
+                                                                $price = null;
+
+                                                                $qty = (int) $item->quantity;
+                                                                if(($var_price->min_qty <= $qty) && ($qty <= $var_price->max_qty)) {
+                                                                    if($var_price->type == 'fixed') {
+                                                                        $price = $var_price->price * ($qty -1);
+                                                                        $price = number_format($price, 2);
+                                                                        $total_discount = $total_discount + $price;
+                                                                        $price = $price . " USD";
+                                                                    }
+                                                                    else if($var_price->type == 'discount') {
+                                                                        $discount = (double) $var_price->price;
+                                                                        $price = $item->cost - ($item->price * $discount / 100);
+                                                                        $price = $price * ($qty -1);
+                                                                        $price = number_format($price, 2);
+                                                                        $total_discount = $total_discount + $price;
+                                                                        $price = $price . " USD";
+                                                                    }
+                                                                }
+                                                                else {
+                                                                    $price = '';
+                                                                }
+                                                            @endphp
+                                                            {{ ($price) }}
+                                                        @endforeach
+                                                    @else
+                                                        <span></span>
+                                                    @endif
+                                                @else
+                                                    <span></span>
+                                                @endif
+
+                                                @if($is_general_discount && $is_applied_for_general_dsiscount)
+                                                    {{ \App\GeneralDiscountPreferences::first()->discount_amount }} % on whole order
+                                                @endif
+
+                                                @if($is_general_discount && $is_applied_for_general_fixed)
+                                                    {{ number_format(\App\GeneralFixedPricePreferences::first()->fixed_amount * ($n - 1), 2) }} $ off on whole order
+                                                @endif
+
+                                            </td>
                                             <td>{{$item->price}} X {{$item->quantity}}  USD </td>
                                             <td>
                                                 @if($item->fulfillment_status == null)
