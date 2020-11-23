03d3c7b4e3ed41418f481fb606145602f4aa9abb
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 14:26:20 +0500

    Fixing SKU csv bug

--- resources/views/exports/variant_sku.blade.php
+++ resources/views/exports/variant_sku.blade.php
@@ -14,24 +14,7 @@
             <td class="font-w600">{{ $v->sku }}</td>
             <td class="font-w600">@if($v->option1 != null) {{$v->option1}} @endif    @if($v->option2 != null) / {{$v->option2}} @endif    @if($v->option3 != null) / {{$v->option3}} @endif</td>
             <td>
-                @php
-                    if($v->has_image == null) {
-                        $img = 'No Image Found';
 
-                    }
-                    else {
-                        if($v->has_image->isV == 0) {
-                            $img=   $v->has_image->image;
-
-                        }
-                        else {
-                            $img=   $v->has_image->image;
-
-                        }
-
-                    }
-                @endphp
-                {{ $img }}
             </td>
         </tr>
     @endforeach
