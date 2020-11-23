eae60ae91c7754440347fa82b582e25806379b13
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-23 13:33:38 +0500

    Removing Login/Register by store

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,17 +9,17 @@
         }
     </style>
     <div class="block">
-        <li class="nav-item">
-            <a class="nav-link active">Login</a>
-        </li>
-{{--        <ul class="nav nav-tabs nav-tabs-block nav-justified" data-toggle="tabs" role="tablist">--}}
+        <ul class="nav nav-tabs nav-tabs-block nav-justified" data-toggle="tabs" role="tablist">
+            <li class="nav-item text-right">
+                <a class="nav-link active">Login</a>
+            </li>
 {{--            <li class="nav-item">--}}
 {{--                <a class="nav-link active" href="#by_email">By Account</a>--}}
 {{--            </li>--}}
 {{--            <li class="nav-item">--}}
 {{--                <a class="nav-link" href="#by_store">By Store</a>--}}
 {{--            </li>--}}
-{{--        </ul>--}}
+        </ul>
         <div class="block-content tab-content">
             <div class="tab-pane active" id="by_email" role="tabpanel">
                 <div class="py-3">
