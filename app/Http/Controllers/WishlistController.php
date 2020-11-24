6ce648c031b268afbb0e45a9dbccb5decfe42f77
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-24 19:19:53 +0500

    Fixing bug related to order fulfillment

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -336,7 +336,7 @@ class WishlistController extends Controller
                 $product =  $this->create_sync_product_to_admin($request, $response);
                 /*Import Product to requested store*/
 
-                dd(234);
+
 
                 $related_product_id = $this->import_to_store($wish,$request->input('product_shopify_id'),$product->id);
                 $wish->status_id = 5;
