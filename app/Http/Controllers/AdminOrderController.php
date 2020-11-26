0f238291516e860e508b60a8940188f3fd3c2cbb
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-26 14:43:09 +0500

    Fixing Order Fullfillment Issue

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -142,6 +142,7 @@ class AdminOrderController extends Controller
                             }
                             $response = $shop->api()->rest('POST', '/admin/orders/' . $order->shopify_order_id . '/fulfillments.json', $data);
                             if ($response->errors) {
+                                dd($response->body);
                                 $res = $shop->api()->rest('GET', '/admin/orders/' . $order->shopify_order_id . '/fulfillments.json');
                                 dd($res);
                                 return redirect()->back()->with('error', 'Cant Fulfill Items of Order in Related Store!');
