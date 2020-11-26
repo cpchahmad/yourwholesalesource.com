12541bf9dd76b5035e36b6994f1e7d7ebe4a40f1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-26 12:23:53 +0500

    "Test"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <div class="content content-full pt-2 pb-2">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill h4 my-2">
-                    Das
+                    Dashboard
                 </h1>
             </div>
         </div>
