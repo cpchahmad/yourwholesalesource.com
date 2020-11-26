1ee763bb0cf199bbee24c50e51ac4d81673e0b40
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-26 15:00:08 +0500

    Fixing Order Fullfillment Issue

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -142,6 +142,7 @@ class AdminOrderController extends Controller
                             }
                             $response = $shop->api()->rest('POST', '/admin/orders/' . $order->shopify_order_id . '/fulfillments.json', $data);
                             if ($response->errors) {
+                                dd($response);
                                 if(strpos($response->body->base[0], "is already fulfilled") !== false){
                                     $res = $shop->api()->rest('GET', '/admin/orders/' . $order->shopify_order_id . '/fulfillments.json');
                                     return $this->set_fulfilments($request, $id, $fulfillable_quantities, $order, $res);
