47093b0f4e586d892533467141fdb3f56d119d84
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-27 18:41:00 +0500

    Checking Issues with Wishlist Completed Email Not Sending Issue

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -350,6 +350,7 @@ class WishlistController extends Controller
                     Mail::to($user->email)->send(new WishlistComplateMail($user, $wish));
                 }
                 catch (\Exception $e){
+                    dd($e);
                 }
 
                 $this->notify->generate('Wish-list','Wishlist Completed','Wishlist named '.$wish->product_name.' has been completed',$wish);
