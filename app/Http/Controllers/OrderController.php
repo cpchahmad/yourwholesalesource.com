c7158fc6db2a3f89a3e7b231a0015a058d1f8f7d
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-30 14:52:17 +0500

    Fixing issues with orders pushing having two or more stores

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -687,6 +687,7 @@ class OrderController extends Controller
         $response = $shop->api()->rest('GET', '/admin/api/2019-10/orders.json', ['status' => 'any']);
         if (!$response->errors) {
             $orders = $response->body->orders;
+            dd(213, $orders);
             foreach ($orders as $index => $order) {
                 $order = $orders[26];
 
