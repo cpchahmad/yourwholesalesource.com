3d10859fc48e055f43f48569168864983f76f237
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-30 18:33:27 +0500

    Fixing order push issue for store Id 15

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -688,7 +688,7 @@ class OrderController extends Controller
         $response = $shop->api()->rest('GET', '/admin/api/2019-10/orders.json', ['status' => 'any']);
         if (!$response->errors) {
             $orders = $response->body->orders;
-            dd(213, $orders[0]);
+            dd(25, $orders);
             foreach ($orders as $index => $order) {
                 $order = $orders[7];
 
