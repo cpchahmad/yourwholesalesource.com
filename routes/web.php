2b42809113a6b9196a6570359ea3bcc50ed465bd
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-11-30 11:58:48 +0500

    Fixing issues with orders pushing having two or more stores

--- routes/web.php
+++ routes/web.php
@@ -359,7 +359,7 @@ Route::get('test/emails', 'HelperController@testEmail');
 
 
 //Route::get('/test', 'AdminOrderController@changeFulfillmentServiceUrl');
-Route::get('/test', 'WishlistController@test');
+//Route::get('/test', 'WishlistController@test');
 //Route::get('/test', function() {
 //    return view('emails.order_status')->with('order', RetailerOrder::find(1))->with('template', EmailTemplate::find(4));
 //});
@@ -373,6 +373,8 @@ Route::get('/test', 'WishlistController@test');
 //Route::get('/test', function() {
 //    return view('emails.top_products_new')->with('top_products_stores', \App\Product::all())->with('template', EmailTemplate::find(13));
 //});
+
+Route::get('/test', 'OrderController@manuallyGetOrders');
 
 
 Route::get('pages', 'AdminMaintainerController@getPages');
