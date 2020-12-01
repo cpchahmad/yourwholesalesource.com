704eef9f41145d537b0716b159b7eb8c142c087e
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-01 12:02:48 +0500

    Fixing Issues with bostin store order pushing

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -688,7 +688,7 @@ class OrderController extends Controller
         $response = $shop->api()->rest('GET', '/admin/api/2019-10/orders.json', ['status' => 'any']);
         if (!$response->errors) {
             $orders = $response->body->orders;
-            dd($orders);
+            dd($orders[0]);
             foreach ($orders as $index => $order) {
                 $order = $orders[3];
 
