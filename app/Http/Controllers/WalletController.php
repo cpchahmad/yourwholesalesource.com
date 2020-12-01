f5acce8d4dd9815cdae09d594ab64c5de4749f60
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-01 15:53:38 +0500

    Adding Activity Logs Tracking Feature

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -250,7 +250,7 @@ class WalletController extends Controller
             $wallet_log->message = 'A Top-up of Amount '.number_format($request->input('amount'),2).' USD edit Against Wallet ' . $get_wallet->wallet_token . ' At ' . now()->format('d M, Y h:i a'). ' By Administration';
             $wallet_log->save();
 
-            $this->log->store(0, 'Wallet', $wallet->id, $wallet->owner()->name,'Wallet Request Edited');
+            $this->log->store(0, 'Wallet', $wallet->id, $wallet->user,'Wallet Request Edited');
 
 
 
