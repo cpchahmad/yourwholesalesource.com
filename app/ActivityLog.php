9058de34ef39608d9168b09644fd22e0cb4510a6
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-01 13:23:40 +0500

    Adding Activity Logs Tracking Feature

--- app/ActivityLog.php
+++ app/ActivityLog.php
@@ -11,6 +11,6 @@ class ActivityLog extends Model
     }
 
     public function product() {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class, 'model_id');
     }
 }
