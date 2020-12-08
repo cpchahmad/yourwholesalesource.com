6b89115be5e9d3b5b8a001c0c5a267670f446ff5
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-08 19:21:24 +0500

    Debugging Variant Images Bug

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -580,12 +580,6 @@ class WishlistController extends Controller
 
         $variants_array = [];
         foreach ($prod->hasVariants as $index => $varaint) {
-            if($varaint->has_image !== null) {
-                $img_id = $varaint->has_image->shopify_id;
-            }
-            else{
-                $img_id = null;
-            }
             array_push($variants_array, [
                 'title' => $varaint->title,
                 'sku' => $varaint->sku,
@@ -603,7 +597,6 @@ class WishlistController extends Controller
                 'barcode' => $varaint->barcode,
                 'price' => $varaint->price,
                 'cost' => $varaint->cost,
-                'image_id' => $img_id,
             ]);
         }
         return $variants_array;
@@ -792,7 +785,6 @@ class WishlistController extends Controller
 
 
         $response = $shop->api()->rest('POST', '/admin/products.json', $productdata);
-        dd($response);
         $product_shopify_id = $response->body->product->id;
         $product->shopify_id = $product_shopify_id;
         $price = $product->price;
@@ -861,20 +853,19 @@ class WishlistController extends Controller
 //                $image->save();
 //            }
 //        }
-//        foreach ($product->hasVariants as $index => $v) {
-//            dump($v);
-//            if ($v->has_image != null) {
-//                $i = [
-//                    'image' => [
-//                        'id' => $v->has_image->shopify_id,
-//                        'variant_ids' => [$v->shopify_id],
-//                        'src' => $v->has_image->image
-//                    ]
-//                ];
-//                $imagesResponse = $shop->api()->rest('POST', '/admin/api/2019-10/products/' . $product_shopify_id . '/images.json', $i);
-//                dump($imagesResponse);
-//            }
-//        }
+        foreach ($product->hasVariants as $index => $v) {
+            dump($v);
+            if ($v->has_image != null) {
+                $i = [
+                    'image' => [
+                        'variant_ids' => [$v->shopify_id],
+                        'src' => $v->has_image->image
+                    ]
+                ];
+                $imagesResponse = $shop->api()->rest('POST', '/admin/api/2019-10/products/' . $product_shopify_id . '/images.json', $i);
+                dump($imagesResponse);
+            }
+        }
         return $product;
     }
 
