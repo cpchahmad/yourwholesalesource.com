f71f7f47f3449050d837c1ab644a7e4eea1436e7
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-10 16:47:48 +0500

    Making Changes To Product Edit Page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -772,7 +772,6 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        dd($request->type);
         $product = Product::find($id);
         $shop =$this->helper->getShop();
         if ($product != null) {
