56052e3f54bfaa5a4eed842c4dae8cf8e734d3a2
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-16 16:28:58 +0500

    Adding filter on user wishlist page

--- routes/web.php
+++ routes/web.php
@@ -401,7 +401,7 @@ Route::get('/testing', function() {
     $shop = $helper->getSpecificShop(108);
     $response = $shop->api()->rest('GET', '/admin/orders.json');
 
-    dd($response->body->orders[11]);
+    dd($response->body->orders);
 });
 
 
