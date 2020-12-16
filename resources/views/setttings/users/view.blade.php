cdaf36cb61f61982a194f83bfb0adf0afd00f7da
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2020-12-16 12:43:04 +0500

    Working on Order Stock Issue

--- resources/views/setttings/users/view.blade.php
+++ resources/views/setttings/users/view.blade.php
@@ -187,15 +187,11 @@
                                                 <span>{{ $out_of_stock }}</span>
 
                                                 @if($order->line_items->where('fulfilled_by', 'store')->count() > 0)
-                                                    @if($order->line_items()->where('fulfilled_by', 'fantasy')->count() == $out_of_stock)
-                                                        <span class="badge badge-danger" style="font-size: small"> Out of Stock </span>
-                                                    @else
-                                                        <span class="badge badge-warning" style="font-size: small"> Partial Out of Stock </span>
-                                                    @endif
+                                                    <span class="badge badge-warning" style="font-size: small"> Partial Out of Stock </span>
                                                 @else
                                                     @if($out_of_stock == 0)
                                                     <span class="badge badge-success" style="font-size: small"> In Stock </span>
-                                                    @elseif($order->line_items()->count() == $out_of_stock)
+                                                    @elseif($order->line_items()->where('fulfilled_by', 'fantasy')->count() == $out_of_stock)
                                                         <span class="badge badge-danger" style="font-size: small"> Out of Stock </span>
                                                     @else
                                                         <span class="badge badge-warning" style="font-size: small"> Partial out of Stock </span>
