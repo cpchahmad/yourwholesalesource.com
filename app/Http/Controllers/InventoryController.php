90aca63562b45ef4598195c2ef318579de816297
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-04 18:11:58 +0500

    Pushing Orders to Mabang ERP

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -172,8 +172,8 @@ class InventoryController extends Controller
                         $variant->quantity = $variant->quantity + $item->quantity;
                     }
                     $variant->save();
-//                    $variant->linked_product = $variant->linked_product->varaint_count($variant->linked_product);
-//                    $variant->linked_product->save();
+                    $variant->linked_product->quantity = $variant->linked_product->varaint_count($variant->linked_product);
+                    $variant->linked_product->save();
                     Artisan::call('app:sku-quantity-change',['product_id'=> $variant->product_id]);
                 }
                 else{
