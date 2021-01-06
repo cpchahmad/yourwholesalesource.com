611671858c494f9692c28db50d583ef9a1abdcfa
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-06 12:31:04 +0500

    Adding Edit Campaign Option

--- resources/views/setttings/campaigns/edit.blade.php
+++ resources/views/setttings/campaigns/edit.blade.php
@@ -21,63 +21,69 @@
         <div class="row" style="margin-top: 10px">
             <div class="col-md-12">
                 <div class="block">
-                    <div class="block-content">
-                        <div class="form-group">
-                            <label for="">Subject</label>
-                            <input type="text" name="subject" class="form-control" value="{{ $template->subject }}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="">Campaign Name</label>
-                            <input type="text" name="campaign_name" class="form-control" value="{{ $campaign->name }}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="">Campaign Time</label>
-                            <input type="text" required name="time" placeholder="{{ $campaign->time }}" class="js-flatpickr form-control bg-white" id="example-flatpickr-datetime-24" name="example-flatpickr-datetime-24" data-enable-time="true" data-time_24hr="true">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="">Body</label>
-                            <textarea name="body" id="" cols="30" rows="10" class="form-control">{{ $template->body }}</textarea>
-                        </div>
-
-                        @if($template->banner !== null)
-                            <div class="text-center">
-                                <img style="width: 50%; height: auto;" src="{{asset('ticket-attachments')}}/{{$template->banner}}" alt="">
+                    <form action="{{}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="block-content">
+                            <div class="form-group">
+                                <label for="">Subject</label>
+                                <input type="text" name="subject" class="form-control" value="{{ $template->subject }}">
                             </div>
 
                             <div class="form-group">
-                                <label for="">Banner</label>
-                                <div class="input-group">
-                                    <div class="custom-file">
-                                        <input name="banner" type="file" class="custom-file-input" id="inputGroupFile04">
-                                        <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                                <label for="">Campaign Name</label>
+                                <input type="text" name="campaign_name" class="form-control" value="{{ $campaign->name }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="">Campaign Time</label>
+                                <input type="text" required name="time" placeholder="{{ $campaign->time }}" class="js-flatpickr form-control bg-white" id="example-flatpickr-datetime-24" name="example-flatpickr-datetime-24" data-enable-time="true" data-time_24hr="true">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="">Body</label>
+                                <textarea name="body" id="" cols="30" rows="10" class="form-control">{{ $template->body }}</textarea>
+                            </div>
+
+                            @if($template->banner !== null)
+                                <div class="text-center">
+                                    <img style="width: 50%; height: auto;" src="{{asset('ticket-attachments')}}/{{$template->banner}}" alt="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="">Banner</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input name="banner" type="file" class="custom-file-input" id="inputGroupFile04">
+                                            <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endif
+                            @endif
 
-                        @if($template->products != null)
-                            <div class="text-left">
-                                <label for="" style="color: #7daa40 !important;">Edit Products</label>
-                            </div>
-                            @php $products = \App\Product::all(); @endphp
-                            <select class="@error('type') is-invalid @enderror js-select2 form-control" name="products[]" style="width: 100%; border-radius: 0 !important;" data-placeholder="Edit Products.." multiple>
-                                @foreach($products as $product)
-                                    @php
-                                        $prods = json_decode($template->products);
-                                    @endphp
-                                    <option value="{{ $product->id }}"
-                                            @if(in_array($product->id, $prods))
-                                            selected
-                                        @endif
-                                    >{{ $product->title }}</option>
-                                @endforeach
+                            @if($template->products != null)
+                                <div class="form-group">
+                                    <label for="">Edit Products</label>
+                                    @php $products = \App\Product::all(); @endphp
+                                    <select class="@error('type') is-invalid @enderror js-select2 form-control" name="products[]" style="width: 100%; border-radius: 0 !important;" data-placeholder="Edit Products.." multiple>
+                                        @foreach($products as $product)
+                                            @php
+                                                $prods = json_decode($template->products);
+                                            @endphp
+                                            <option value="{{ $product->id }}"
+                                                    @if(in_array($product->id, $prods))
+                                                    selected
+                                                @endif
+                                            >{{ $product->title }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            @endif
 
-                            </select>
-                        @endif
-                    </div>
+                            <div class="form-group text-right">
+                                <button class="btn btn-success" type="submit">Update</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
