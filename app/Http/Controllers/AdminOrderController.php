45726dc4beee8d7eb949f331e5c49bdc8a2ac9fd
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-07 14:26:25 +0500

    Adding Update Tracking Info Feature Backend API integration

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -372,6 +372,7 @@ class AdminOrderController extends Controller
 
 
                                 $response = $shop->api()->rest('POST', '/admin/fulfillments/' . $current->fulfillment_shopify_id . '/update_tracking.json', $data);
+                                dump($response);
                                 if ($order->admin_shopify_id != null) {
                                     $this->admin_maintainer->admin_order_fulfillment_edit_tracking($order, $current, $data);
                                 }
