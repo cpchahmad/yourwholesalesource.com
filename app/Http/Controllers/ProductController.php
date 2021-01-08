a108adeceed646c3ef9981d729473aa7234ffbd1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-08 18:20:08 +0500

    Filtering Products based on category front end

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -79,7 +79,7 @@ class ProductController extends Controller
 
         if($request->filled('parent_category') && $request->filled('child_category')) {
 
-            $productQ->orWhereHas('has_categories', function($q) use ($request){
+            $productQ->whereHas('has_categories', function($q) use ($request){
                 $q->where('title',$request->input('parent_category'))
                     ->whereHas('hasSub', function($inner) use ($request) {
                         $inner->where('title',$request->input('child_category'));
