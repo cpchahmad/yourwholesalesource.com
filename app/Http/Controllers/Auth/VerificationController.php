d8d7b35bc984189a25083b75465b55da4098c500
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-11 18:03:37 +0500

    Addinng Email Verification

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/loginEventServiceProvider';
 
     /**
      * Create a new controller instance.
