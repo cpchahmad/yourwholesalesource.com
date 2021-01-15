10526b2624d84d37c5b8b0b8b04386dffaaf391f
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-15 12:52:06 +0500

    Adding Courier Name on shop side

--- resources/views/single-store/orders/view.blade.php
+++ resources/views/single-store/orders/view.blade.php
@@ -659,7 +659,9 @@
                             </div>
                             <div class="block-content">
                                 @if($order->status == "shipped")
-                                    <p style="font-size: 12px"> Tracking Number : {{$fulfillment->tracking_number}} <br>
+                                    <p style="font-size: 12px">
+                                        Courier Service Provider : @if($fulfillment->courier ) {{ $fulfillment->courier->title }} @endif <br>
+                                        Tracking Number : {{$fulfillment->tracking_number}} <br>
                                         Tracking Url : {{$fulfillment->tracking_url}} <br>
                                         Tracking Notes : {{$fulfillment->tracking_notes}} <br>
                                     </p>
