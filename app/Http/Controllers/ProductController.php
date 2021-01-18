1b3565cd604eeff7a91ec6b71072eeda31c1647a
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-18 15:09:19 +0500

    changing logic of edit tab modal

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1210,7 +1210,7 @@ class ProductController extends Controller
 
 
             dump($product->woocommerce_id);
-            $data = ['regular_price' => '40.54', 'meta_data' => null];
+            $data = ['regular_price' => '49.54', 'meta_data' => null, 'categories' => array()];
             $resp =  $woocommerce->put('products/'.$product->woocommerce_id, $data);
             dump(13,$resp);
             $data = ['meta_data' => $updated_array];
