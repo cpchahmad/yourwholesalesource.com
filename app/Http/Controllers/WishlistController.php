8525e42f2fc77cd65f9d15a38e16c0a766c74957
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-01-19 18:17:45 +0500

    Pushing Wishlist product to woocommerce

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -348,7 +348,6 @@ class WishlistController extends Controller
                 $product =  $this->create_sync_product_to_admin($request, $response);
                 /*Import Product to requested store*/
 
-                dd(123);
                 $related_product_id = $this->import_to_store($wish,$request->input('product_shopify_id'),$product->id);
                 $wish->status_id = 5;
                 $wish->related_product_id = $related_product_id;
