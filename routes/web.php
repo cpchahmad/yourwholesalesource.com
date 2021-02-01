8a94034ee2ba8780b91f5a9ab44c393f2368bd00
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-01 13:15:45 +0500

    Manually pushing old orders who are already fullfilled

--- routes/web.php
+++ routes/web.php
@@ -453,5 +453,6 @@ Route::any('/order/fulfillment/details', 'AdminOrderController@getFulfillmentFro
 
 Route::get('bulk', 'AdminMaintainerController@bulk_import_to_woocommerce');
 
-Route::get('/testing', 'OrderController@test');
+
+Route::get('/dummy', 'AdminOrderController@dummy');
 
