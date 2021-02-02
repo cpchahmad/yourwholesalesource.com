7ad7d627b8b874b1b7c5f8bf815253e04de19052
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-02 11:38:18 +0500

    Fixing buggy order 1455

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -1656,7 +1656,7 @@ class AdminOrderController extends Controller
                             $response = $shop->api()->rest('GET','/admin/orders/'.$retailer_order->shopify_order_id.'/fulfillments.json',$fulfill_data);
                             if(!$response->errors){
 
-                                dd($location_response);
+                                dd($response);
 
                                 /*Order Fullfillment Record*/
                                 $new_fulfillment = new OrderFulfillment();
