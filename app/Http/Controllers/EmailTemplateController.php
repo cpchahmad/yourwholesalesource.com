f5fe382129abc9057be02d14bdc54083432fc01f
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-03 16:03:35 +0500

    Adding Wallet Approve email template

--- app/Http/Controllers/EmailTemplateController.php
+++ app/Http/Controllers/EmailTemplateController.php
@@ -68,6 +68,8 @@ class EmailTemplateController extends Controller
                 return view('emails.wishlist_approve')->with('template', EmailTemplate::find(8))->with('wishlist', Wishlist::find(1));
             case 9:
                 return view('emails.wishlist_reject')->with('template', EmailTemplate::find(9))->with('wishlist', Wishlist::find(1));
+            case 10:
+                return view('emails.wallet_approve')->with('template', EmailTemplate::find(10))->with('wallet', Wallet::find(1));
             case 12:
                 return view('emails.wishlist_complete')->with('template', EmailTemplate::find(12))->with('wishlist', Wishlist::find(1));
             case 19:
