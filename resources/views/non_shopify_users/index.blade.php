f52b4f04dcf9934dcc275827deb7f9f863bf7e21
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-04 20:19:11 +0500

    Adding Sidebar on dashboard

--- resources/views/non_shopify_users/index.blade.php
+++ resources/views/non_shopify_users/index.blade.php
@@ -166,7 +166,7 @@
                     @endphp
                     @if($manager)
                         <div class="block-content" >
-                            <div class="media-body">
+                            <div class="media-body pb-3 text-center">
                                 <img class="img-avatar-rounded" @if($manager->profile == null) src="{{ asset('assets/media/avatars/avatar10.jpg') }}" @else  src="{{asset('managers-profiles')}}/{{$manager->profile}}" @endif alt="Header Avatar" style="width: 18px;">
                                 <div class="font-w600">{{$manager->name}} {{$manager->last_name}}</div>
                                 <div class="font-w600">{{$manager->email}}</div>
@@ -186,21 +186,13 @@
                 <div class="block">
                     <div class="block-header">
                         <div class="block-title">
-                            Mark as Fulfilled
+                            Your Wallet Balance
                         </div>
                     </div>
                     <input type="hidden" name="type[]" value="fulfilled">
                     <div class="block-content" >
-                        <div class="form-group">
-                            <div class="custom-control custom-radio mb-1">
-                                <input type="radio" required class="custom-control-input" id="example-radio-customFantasy"  name="fulfilled-by" value="Fantasy"  >
-                                <label class="custom-control-label" for="example-radio-customFantasy">By WeFullFill</label>
-                            </div>
-                            <div class="custom-control custom-radio mb-1">
-                                <input type="radio" required class="custom-control-input" id="example-radio-customAliExpress" name="fulfilled-by" value="AliExpress"  >
-                                <label class="custom-control-label" for="example-radio-customAliExpress">By AliExpress</label>
-                            </div>
-                        </div>
+                        <div class="font-w600 text-left">Wallet Balance</div>
+                        <div class="mt-2 p-2 bg-primary text-white">{{number_format($balance,2)}} USD</div>
                     </div>
                 </div>
                 <div class="block">
