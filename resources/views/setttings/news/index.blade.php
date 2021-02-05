4e336fa98e07fea336205a426af0b5430a4b398f
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-05 14:33:06 +0500

    Adding News Crud on admin side

--- resources/views/setttings/news/index.blade.php
+++ resources/views/setttings/news/index.blade.php
@@ -129,7 +129,7 @@
                                         <div class="modal-content">
                                             <div class="block block-themed block-transparent mb-0">
                                                 <div class="block-header bg-primary-dark">
-                                                    <h3 class="block-title">Edit "{{ $news->title}}"</h3>
+                                                    <h3 class="block-title">Edit "{{ $item->title}}"</h3>
                                                     <div class="block-options">
                                                         <button type="button" class="btn-block-option">
                                                             <i class="fa fa-fw fa-times"  data-dismiss="modal" aria-label="Close"></i>
