167d2da48b8adb93be86cd92baef3d6e224f72fd
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-08 18:33:49 +0500

    Adding user filters on email template 20

--- app/Http/Controllers/EmailTemplateController.php
+++ app/Http/Controllers/EmailTemplateController.php
@@ -138,7 +138,19 @@ class EmailTemplateController extends Controller
             $campaign->template_id = $template->id;
             //$campaign->save();
 
-            $users_id = array_merge($request->users, $request->shopify_users, $request->shopify_users_with_orders, $request->non_shopify_users_with_orders, $request->users_with_products, $request->users_without_products);
+            if($request->users)
+                $users_id = array_merge($request->users);
+            if($request->shopify_users)
+                $users_id = array_merge($request->shopify_users);
+            if($request->shopify_users_with_orders)
+                $users_id = array_merge($request->shopify_users_with_orders);
+            if($request->non_shopify_users_with_orders)
+                $users_id = array_merge($request->non_shopify_users_with_orders);
+            if($request->users_with_products)
+                $users_id = array_merge($request->users_with_products);
+            if($request->users_without_products)
+                $users_id = array_merge($request->users_without_products);
+
 
             dd($request->all(), $users_id);
 
