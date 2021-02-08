ffbc02d5e5ffb478add0fd4f5ab6d400b2087cd6
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-08 14:00:02 +0500

    Making new dashboard for non-shopify user side

--- app/Http/Middleware/CheckUserQuestionaire.php
+++ app/Http/Middleware/CheckUserQuestionaire.php
@@ -22,17 +22,16 @@ class CheckUserQuestionaire
         if($user != null){
             $filled_questionnaire = Questionaire::where('user_id',$user->id)->first();
             if($filled_questionnaire == null){
-                dd(324);
-                return redirect()->route('users.dashboard',['ftl' => '1'])->with('failure', 'Kindly fill the Questionaire to continue');
-            }
-            else if(count($user->has_shops) > 0){
-                $array = $user->has_shops->pluck('id')->toArray();
-                $filled_questionnaire = Questionaire::whereIn('shop_id',$array)->first();
-                if($filled_questionnaire == null){
-                    return redirect()->route('users.dashboard',['ftl' => '1'])->with('failure', 'Kindly fill the Questionaire to continue');
+                if(count($user->has_shops) > 0){
+                    $array = $user->has_shops->pluck('id')->toArray();
+                    $filled_questionnaire = Questionaire::whereIn('shop_id',$array)->first();
+                    if($filled_questionnaire == null)
+                        return redirect()->route('users.dashboard',['ftl' => '1'])->with('failure', 'Kindly fill the Questionaire to continue');
+                    else
+                        return $next($request);
                 }
                 else{
-                    return $next($request);
+                    return redirect()->route('users.dashboard',['ftl' => '1'])->with('failure', 'Kindly fill the Questionaire to continue');
                 }
             }
             else{
