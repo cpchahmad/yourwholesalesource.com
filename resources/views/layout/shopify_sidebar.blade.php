c7265fcd7d146628f23815f43c7ae9cbace0f4c5
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-08 13:34:29 +0500

    Making new dashboard for non-shopify user side

--- resources/views/layout/shopify_sidebar.blade.php
+++ resources/views/layout/shopify_sidebar.blade.php
@@ -146,7 +146,6 @@
         <div class="d-flex align-items-center">
             <div class="d-inline-block mr-3">
                 <span class="badge badge-primary" style="font-size: 13px"> Wallet Balance  : {{number_format($balance,2)}} USD </span>
-                <span class="badge badge-primary" style="font-size: 13px"> Wallet Balance  : {{number_format($balance,2)}} USD </span>
                 <span class="badge badge-info" style="font-size: 13px"> Approved Wishlist  {{$approved_wishlist}}  </span>
                 <span class="badge badge-success" style="font-size: 13px"> Completed Wishlist  {{$completed_wishlist}}  </span>
                 <span class="badge badge-dark" style="font-size: 13px"> Pending Tickets  {{$pending_ticket_count}}  </span>
