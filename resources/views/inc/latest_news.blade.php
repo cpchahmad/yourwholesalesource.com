de64bb5189a130ed31ca760f61915886cfcd4491
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-09 14:55:36 +0500

    Adding latest news on Top

--- resources/views/inc/latest_news.blade.php
+++ resources/views/inc/latest_news.blade.php
@@ -1,7 +1,7 @@
 @if($latest_news)
     <div>
         <marquee>
-            <h1 class="flex-sm-fill h5 my-2 text-danger">
+            <h1 class="flex-sm-fill h5 my-2 text-city">
                 <i class="fa fa-volume-up text-city" ></i>
                 {{ $latest_news->title }} :
                 <span class="font-size-sm text-muted text-danger">{{ $latest_news->description }}</span>
