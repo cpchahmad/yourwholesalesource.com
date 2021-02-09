f665bd0a6791f57fd65155157807175e669594d2
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-09 20:30:19 +0500

    Adding Show shipping in retailer product edit page

--- resources/views/single-store/products/edit_my_product.blade.php
+++ resources/views/single-store/products/edit_my_product.blade.php
@@ -262,7 +262,7 @@
                                                 <input type="text" class="form-control" name="price" placeholder="$0.00" value="{{$v->price}}">
                                             </td>
                                             <td><input type="text" class="form-control" readonly value="{{$v->cost}}" placeholder="$0.00"></td>
-                                            <td class="drop-shipping text-center"><input type="text" value="N/A"></td>
+                                            <td class="text-center"><input type="text" value="N/A"></td>
                                             <td><input type="text" readonly class="form-control" value="{{$v->quantity}}" name="quantity" placeholder="0"></td>
                                             <td><input type="text" readonly class="form-control" name="sku" value="{{$v->sku}}"></td>
                                             <td><input type="text" class="form-control" name="barcode" value="{{$v->barcode}}" placeholder="">
