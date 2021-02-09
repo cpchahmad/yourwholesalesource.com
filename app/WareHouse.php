9a460886ad7fbc360db1a8247d9078547ee644bc
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-09 19:59:20 +0500

    Adding Videos to non-shopify user

--- app/WareHouse.php
+++ app/WareHouse.php
@@ -26,12 +26,12 @@ class WareHouse extends Model
 
         $real_variants = $product->hasVariants()->pluck('id')->toArray();
 
-        if(WarehouseInventory::where('warehouse_id', $this->id)->whereIn('product_variant_id', $real_variants)->exists()) {
+        if(WarehouseInventory::where('warehouse_id', $this->id)->whereIn('product_variant_id', $real_variants)->exists())
             $flag = true;
-        }
-        if(WarehouseInventory::where('warehouse_id', $this->id)->where('product_id', $product->id)->exists()) {
+
+        if(WarehouseInventory::where('warehouse_id', $this->id)->where('product_id', $product->id)->exists())
             $flag = true;
-        }
+
         return $flag;
     }
 
