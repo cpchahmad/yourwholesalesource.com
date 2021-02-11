99d3a05cb7b0cdc42b96df58e6964e01dd834ee9
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-11 17:25:51 +0500

    Showing Monthly discount on admin side

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -464,6 +464,15 @@
                                         if($is_general_discount && $is_applied_for_general_fixed) {
                                            $total_discount = (double) \App\GeneralFixedPricePreferences::first()->fixed_amount * ($n - 1);
                                         }
+
+                                        if($is_monthly_discount) {
+                                               $discount = (double) \App\MonthlyDiscountSetting::first()->discount;
+                                               $price = $order->cost_to_pay - ($order->cost_to_pay * $discount / 100);
+                                               $price = number_format($price, 2);
+                                               $total_discount = $total_discount + $price;
+                                               $total_discount = $order->cost_to_pay - $total_discount;
+                                        }
+
                                     @endphp
                                     {{ number_format($total_discount,2) }} USD
                                 </td>
