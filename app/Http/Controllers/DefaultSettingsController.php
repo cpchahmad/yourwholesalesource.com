c4fb5d4951f6fc9099374c496580c19975b43a08
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-11 14:06:36 +0500

    Adding Monthly Sales Setting on admin side (Enable Flag)

--- app/Http/Controllers/DefaultSettingsController.php
+++ app/Http/Controllers/DefaultSettingsController.php
@@ -706,7 +706,6 @@ class DefaultSettingsController extends Controller
         $settings->enable = $request->enable ? 1 : 0;
         $settings->save();
 
-        dd($request->all());
 
         return redirect()->back()->with('success', 'Monthly Discount Settins Saved Successfully!');
     }
