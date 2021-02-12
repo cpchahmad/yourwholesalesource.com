2f626f9aece4f2a68bdaf3cf1da4d29f47b5372b
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-12 11:20:05 +0500

    checking issue with 1321 mega deal warehouse order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -198,6 +198,8 @@ class OrderController extends Controller
 
             if (!$response->errors) {
                 $orders = $response->body->orders;
+
+                dd($orders);
                 foreach ($orders as $index => $order) {
                     $product_ids = [];
                     $variant_ids = [];
