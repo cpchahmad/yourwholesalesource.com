cfe311e1c418416c29802d1b2e22a29aaa55bec9
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-16 17:46:08 +0500

    Showing warehouse on single store orders page

--- app/Http/Controllers/SingleStoreController.php
+++ app/Http/Controllers/SingleStoreController.php
@@ -725,14 +725,16 @@ class SingleStoreController extends Controller
             return view('inc.warehouse')->with([
                 'shipping' => 'This product is not shipped to this country',
                 'order' => $order,
-                'total' => $order->sub_total
+                'total' => $order->subtotal_price,
+                'status' => 'failure'
             ])->render();
 
         if(!in_array($country, $countries))
             return view('inc.warehouse')->with([
                 'shipping' => 'This product is not shipped to this country',
                 'order' => $order,
-                'total' => $order->sub_total
+                'total' => $order->subtotal_price,
+                'status' => 'failure'
             ])->render();
 
 
@@ -763,7 +765,8 @@ class SingleStoreController extends Controller
         return view('inc.warehouse')->with([
             'shipping' => number_format($shipping_rate->shipping_price, 2) . 'USD',
             'order' => $order,
-            'total' => number_format($order->subtotal_price + $shipping_rate->shipping_price, 2)
+            'total' => number_format($order->subtotal_price + $shipping_rate->shipping_price, 2),
+            'status' => 'success'
         ])->render();
 
 
