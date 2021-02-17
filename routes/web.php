8c6826f43feb4606ac8ce04d2b53bc260eb62b42
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-17 19:33:21 +0500

    Manually adding warehouse inventory for products

--- routes/web.php
+++ routes/web.php
@@ -441,41 +441,43 @@ Route::post('suggestions/create', 'DefaultSettingsController@createSuggestion')-
 //
 
 Route::get('/ware-sync', function() {
-   $p = Product::find(611);
 
+    $products = Product::latest()->get();
 
-   if($p->variants == 1)
-   {
-       $variants = $p->hasVariants;
+    foreach ($products as $p) {
+        if($p->variants == 1)
+        {
+            $variants = $p->hasVariants;
 
-       foreach ($variants as $variant) {
-           if(WarehouseInventory::where('product_variant_id', $variant->id)->where('warehouse_id', 3)->exists()) {
-               $inventory = WarehouseInventory::where('product_variant_id', $variant->id)->where('warehouse_id', 3)->first();
-           }
-           else{
-               $inventory = new WarehouseInventory();
-           }
+            foreach ($variants as $variant) {
+                if(WarehouseInventory::where('product_variant_id', $variant->id)->where('warehouse_id', 3)->exists()) {
+                    $inventory = WarehouseInventory::where('product_variant_id', $variant->id)->where('warehouse_id', 3)->first();
+                }
+                else{
+                    $inventory = new WarehouseInventory();
+                }
 
-           $inventory->product_variant_id = $variant->id;
-           $inventory->warehouse_id = 3;
-           $inventory->quantity = $variant->quantity;
-           $inventory->save();
-       }
-   }
-   else {
+                $inventory->product_variant_id = $variant->id;
+                $inventory->warehouse_id = 3;
+                $inventory->quantity = $variant->quantity;
+                $inventory->save();
+            }
+        }
+        else {
 
-       if(WarehouseInventory::where('product_id', $p->id)->where('warehouse_id', 3)->exists()){
-           $inventory = WarehouseInventory::where('product_id', $p->id)->where('warehouse_id', 3)->first();
-       }
-       else{
-           $inventory = new WarehouseInventory();
-       }
+            if(WarehouseInventory::where('product_id', $p->id)->where('warehouse_id', 3)->exists()){
+                $inventory = WarehouseInventory::where('product_id', $p->id)->where('warehouse_id', 3)->first();
+            }
+            else{
+                $inventory = new WarehouseInventory();
+            }
 
-       $inventory->product_id = $p->id;
-       $inventory->warehouse_id = 3;
-       $inventory->quantity = $p->quantity;
-       $inventory->save();
-   }
+            $inventory->product_id = $p->id;
+            $inventory->warehouse_id = 3;
+            $inventory->quantity = $p->quantity;
+            $inventory->save();
+        }
+    }
 
 });
 
