8bd24e6d5b450a656d0885259b7ea2b6a1b4d160
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-18 12:09:14 +0500

    Adding new calculate function

--- resources/views/inc/warehouse.blade.php
+++ resources/views/inc/warehouse.blade.php
@@ -174,7 +174,7 @@
             @if($order->paid == 0)
                 {{--                                        <button class="btn btn-success" data-toggle="modal" data-target="#payment_modal"><i class="fa fa-credit-card"></i> Credit Card Pay</button>--}}
                 <button class="btn btn-success paypal-pay-button" data-toggle="modal" data-target="#paypal_pay_trigger" data-href="{{route('store.order.paypal.pay',$order->id)}}" data-percentage="{{$settings->paypal_percentage}}" data-fee="{{number_format($total - $total_discount *$settings->paypal_percentage/100,2)}}" data-subtotal="{{number_format($total,2)}}" data-pay=" {{number_format($total+($total*$settings->paypal_percentage/100),2)}} USD" ><i class="fab fa-paypal"></i> Paypal Pay</button>
-                <button class="btn btn-success wallet-pay-button" data-href="{{route('store.order.wallet.pay',$order->id)}}" data-pay=" {{number_format($total - $total_discount,2)}} USD" d ><i class="fa fa-wallet"></i> Wallet Pay</button>
+                <button class="btn btn-success wallet-pay-button" data-href="{{route('store.order.wallet.pay',$order->id)}}" data-pay=" {{ ($total - $total_discount) }}" d ><i class="fa fa-wallet"></i> Wallet Pay</button>
 
 
                 <div class="modal" id="paypal_pay_trigger" tabindex="-1" role="dialog" aria-labelledby="modal-block-vcenter" aria-hidden="true">
