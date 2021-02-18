6d293eeb6bc7b5cfae6cffaf5769a11cc8b6d282
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-18 17:24:20 +0500

    Adding warehouse on non-shopify side

--- database/migrations/2021_02_18_122352_add_woocommerce_id_to_retailer_order_line_items_table.php
+++ database/migrations/2021_02_18_122352_add_woocommerce_id_to_retailer_order_line_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddWoocommerceProductIdToRetailerOrderLineItemsTable extends Migration
+class AddWoocommerceIdToRetailerOrderLineItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddWoocommerceProductIdToRetailerOrderLineItemsTable extends Migration
     public function up()
     {
         Schema::table('retailer_order_line_items', function (Blueprint $table) {
-            $table->text('woocommerce_product_id')->nullable();
+            $table->text('woocommerce_id')->nullable();
+
         });
     }
 
