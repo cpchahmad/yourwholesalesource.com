ebff5b906919cbdf6f6589f8e1adfd8b611ee0b0
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-19 20:24:34 +0500

    Adding email changing feature on manager

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -201,7 +201,7 @@ class OrderController extends Controller
             if (!$response->errors) {
                 $orders = $response->body->orders;
 
-                dd($orders);
+                dd($orders[4]);
 
                 foreach ($orders as $index => $order) {
                     $product_ids = [];
