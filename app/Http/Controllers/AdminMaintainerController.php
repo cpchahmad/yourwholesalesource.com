9bc2d9210ba24f170dc077bc10dc6353d62e3792
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-19 16:10:38 +0500

    Adding inventory check in InventoryController.php

--- app/Http/Controllers/AdminMaintainerController.php
+++ app/Http/Controllers/AdminMaintainerController.php
@@ -531,9 +531,6 @@ class AdminMaintainerController extends Controller
             "orderItemList" => $line_items
         ];
 
-        dd($data);
-
-
         $data['phone1'] =  isset($shipping->phone) && $shipping->phone != "" ? $shipping->phone : 'No Phone';
         $data['country'] = is_null($shipping->country) ? 'No country' : $shipping->country;
         $data['street1'] = is_null($shipping->address1) ? 'No First Address' : $shipping->address1;
