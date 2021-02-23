3e1fdb10c576bdbd15afeebe9f7dd80c0a7c5dbb
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-23 18:29:48 +0500

    Fixing Top navbar for admin

--- resources/views/layout/single_sidebar.blade.php
+++ resources/views/layout/single_sidebar.blade.php
@@ -191,15 +191,15 @@
         <div class="d-flex align-items-center">
             <!-- User Dropdown -->
             <div class="">
-                <span class="badge badge-primary" style="font-size: 13px"> Wallet Balance  : {{number_format($balance,2)}} USD </span>
+                <span class="badge badge-primary" style="font-size: 13px"> Wallet: {{number_format($balance,2)}} USD </span>
                 <a href="/store/wishlist?status=2" class="text-white">
-                    <span class="badge badge-info" style="font-size: 13px"> Approved Wishlist  {{$approved_wishlist}} </span>
+                    <span class="badge badge-info" style="font-size: 13px"> Approved Wishlist {{$approved_wishlist}} </span>
                 </a>
                 <a href="/store/wishlist?status=5&imported=0" class="text-white">
-                    <span class="badge badge-success" style="font-size: 13px"> Completed Wishlist  {{$completed_wishlist}}  </span>
+                    <span class="badge badge-success" style="font-size: 13px"> Completed Wishlist {{$completed_wishlist}}  </span>
                 </a>
                 <a href="/store/help-center" class="text-white">
-                    <span class="badge badge-dark" style="font-size: 13px"> Pending Tickets  {{$pending_ticket_count}}  </span>
+                    <span class="badge badge-dark" style="font-size: 13px"> Pending Tickets {{$pending_ticket_count}}  </span>
                 </a>
             </div>
             @if(\Illuminate\Support\Facades\Auth::check())
