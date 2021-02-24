50a2933a098284ac132ec3e695b3616611972713
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-24 19:56:35 +0500

    Adding manager sales report

--- app/Http/Controllers/DefaultSettingsController.php
+++ app/Http/Controllers/DefaultSettingsController.php
@@ -335,13 +335,11 @@ class DefaultSettingsController extends Controller
 
 
                 $active_stores = $manager->has_sales_stores()->get()->filter(function($store) {
-                    if($store->has_orders()->count() > 0 && $store->has_imported()->count() > 0)
-                        return $store;
+                    return $store ?? $store->has_orders()->count() > 0 && $store->has_imported()->count() > 0;
                 });
 
                 $new_stores = $manager->has_sales_stores()->get()->filter(function($store) {
-                    if($store->has_orders()->count() == 0 && $store->has_imported()->count() == 0)
-                        return $store;
+                    return $store ?? $store->has_orders()->count() == 0 && $store->has_imported()->count() == 0;
                 });
 
                 dd($active_stores, $new_stores);
