f115525da99fbba3ab893f16a93d56c3ddf2e455
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-02-24 20:17:37 +0500

    Adding manager sales report

--- resources/views/setttings/sales-managers/reports.blade.php
+++ resources/views/setttings/sales-managers/reports.blade.php
@@ -28,7 +28,7 @@
                 <button class="btn btn-primary filter_by_date" data-url="{{route('sales-managers.report', $manager->id)}}" style="margin-left: 10px"> Filter </button>
             </div>
             <div class="col-md-8 d-flex">
-                <img class=" img-avatar-rounded" @if($manager->profile == null) src="{{ asset('assets/media/avatars/avatar10.jpg') }}" @else  src="{{asset('managers-profiles')}}/{{$manager->profile}}" @endif alt="Header Avatar" style="width: 60px !important; height: 60px !important;">
+                <img class=" img-avatar-rounded" @if($manager->profile == null) src="{{ asset('assets/media/avatars/avatar10.jpg') }}" @else  src="{{asset('managers-profiles')}}/{{$manager->profile}}" @endif alt="Header Avatar" style="width: 50px !important; height: 50px !important;">
                 <div class="d-flex align-items-center ml-2">
                     <div class="font-w600 ml-1">{{$manager->name}} {{$manager->last_name}}</div>
                     <div class="font-w600 ml-1">
