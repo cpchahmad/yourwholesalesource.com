5129cf92d836e2ad73b2d83c8062db71ef4ebfa1
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-03 18:24:13 +0500

    Storing product as admin product on completing dropship request

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -113,6 +113,7 @@ class ProductController extends Controller
 
         return view('products.all')->with([
             'products' => $productQ
+                ->whereNull('is_dropship_product')
                 ->select('id', 'to_woocommerce','title', 'price', 'quantity', 'status', 'woocommerce_id')
                 ->with(['has_images:id,position,image,product_id', 'hasVariants:id,price,product_id'])
                 ->orderBy('created_at','DESC')->paginate(20),
