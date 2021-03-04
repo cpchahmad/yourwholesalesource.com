f9b4150a9590f91746c660b666c661ecf06fc1ba
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-04 18:26:53 +0500

    Showing Dropship Products of user on order create page

--- app/Http/Controllers/CustomOrderController.php
+++ app/Http/Controllers/CustomOrderController.php
@@ -199,7 +199,7 @@ class CustomOrderController extends Controller
             });
         }
         $html = view('non_shopify_users.orders.dropship-product-browse-section')->with([
-            'products' => $products->get()
+            'products' => $products
                 ->limit(10)->get(),
         ])->render();
 
