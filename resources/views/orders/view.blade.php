b0b2dc0bc372fd23f051b1f7ae8351ece90cc19b
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-04 18:50:18 +0500

    Showing Dropship Products of user on order create page

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -206,6 +206,10 @@
                                                     <img class="img-avatar"
                                                          @if($item->linked_real_variant->has_image == null)  src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg"
                                                          @else @if($item->linked_real_variant->has_image->isV == 1) src="{{asset('images/variants')}}/{{$item->linked_real_variant->has_image->image}}" @else src="{{asset('images')}}/{{$item->linked_real_variant->has_image->image}}" @endif @endif alt="">
+                                                @elseif($item->linked_dropship_variant != null)
+                                                    <img class="img-avatar"
+                                                         @if($item->linked_dropship_variant->image == null)  src="https://wfpl.org/wp-content/plugins/lightbox/images/No-image-found.jpg"
+                                                         @else src="{{asset('shipping-marks')}}/{{$item->linked_dropship_variant->image}}" @endif alt="">
                                                 @else
                                                     @if($item->linked_real_product != null)
                                                         @if(count($item->linked_real_product->has_images)>0)
