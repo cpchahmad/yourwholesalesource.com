fcdea880fdfc370abd219b320f2d116f675e84c2
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-11 18:30:48 +0500

    Removing Woocommerce from tags and categrories

--- resources/views/non_shopify_users/orders/view.blade.php
+++ resources/views/non_shopify_users/orders/view.blade.php
@@ -1064,12 +1064,12 @@
     @if($order->paid == 0)
 
 
-{{--        <script src="https://www.paypal.com/sdk/js?client-id=AV6qhCigre8RgTt8E6Z0KNesHxr1aDyJ2hmsk2ssQYmlaVxMHm2JFJvqDCsU15FhoCJY0mDzOu-jbFPY&currency=USD"></script>--}}
+        <script src="https://www.paypal.com/sdk/js?client-id=AV6qhCigre8RgTt8E6Z0KNesHxr1aDyJ2hmsk2ssQYmlaVxMHm2JFJvqDCsU15FhoCJY0mDzOu-jbFPY&currency=USD"></script>
 
 
-        <script
-            src="https://www.paypal.com/sdk/js?client-id=ASxb6_rmf3pte_En7MfEVLPe_KDZQj68bKpzJzl7320mmpV3uDRDLGCY1LaCkyYZ4zNpHdC9oZ73-WFv">
-        </script>
+{{--        <script--}}
+{{--            src="https://www.paypal.com/sdk/js?client-id=ASxb6_rmf3pte_En7MfEVLPe_KDZQj68bKpzJzl7320mmpV3uDRDLGCY1LaCkyYZ4zNpHdC9oZ73-WFv">--}}
+{{--        </script>--}}
         <script>
 
             paypal.Buttons({
