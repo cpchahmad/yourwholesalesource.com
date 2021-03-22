847323f43171ec6a5241b6398cac3c0833e6eaaf
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-22 12:24:59 +0500

    Getting Shipstation store id

--- routes/web.php
+++ routes/web.php
@@ -665,7 +665,32 @@ Route::get('/webhok', function() {
     $response = curl_exec($curl);
 
     curl_close($curl);
-    echo $response;
+    dd($response);
+});
+
+Route::get('storeid', function() {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://ssapi.shipstation.com/stores/storeId",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "Host: ssapi.shipstation.com",
+            "Authorization: Basic MjRkYWZmNmY4OGMzNGY4OGJhZmE0ZWI3ZmRlM2NhNjA6ODkyYjM0MjYzMjgzNDkxNmFlZjAzNWQ5NTc2MzZkNDc=",
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    dd($response);
 });
 
 
