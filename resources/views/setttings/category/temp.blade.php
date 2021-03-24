eaa2cfe469eb47148f2595ddf047231e24fe2236
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-24 20:12:17 +0500

    Adding Sub Sub Categories (Back End)

--- resources/views/setttings/category/temp.blade.php
+++ resources/views/setttings/category/temp.blade.php
@@ -65,19 +65,19 @@
                                     </div>
                                 </div>
                                 <div class="form-group sub_cat_start d-flex">
-                                        <div class="col-sm-10">
-                                            <div class="form-material">
-                                                <label for="material-error">Sub Category</label>
-                                                <input class="form-control" type="text" id="sub_cat" name="sub_title[]"
-                                                       placeholder="Enter Sub Category Title here">
-                                            </div>
+                                    <div class="col-sm-10">
+                                        <div class="form-material">
+                                            <label for="material-error">Sub Category</label>
+                                            <input class="form-control" type="text" id="sub_cat" name="sub_title[]"
+                                                   placeholder="Enter Sub Category Title here">
                                         </div>
-                                        <div class="col-sm-2" style="margin-top: 28px">
-                                            <button class="btn btn-xs btn-default btn-primary sub_cat_btn" type="button"
-                                                    data-toggle="tooltip" title=""
-                                                    data-original-title="Add New SubCategory"><i class="fa fa-plus"></i>
-                                            </button>
-                                        </div>
+                                    </div>
+                                    <div class="col-sm-2" style="margin-top: 28px">
+                                        <button class="btn btn-xs btn-default btn-primary sub_cat_btn" type="button"
+                                                data-toggle="tooltip" title=""
+                                                data-original-title="Add New SubCategory"><i class="fa fa-plus"></i>
+                                        </button>
+                                    </div>
                                 </div>
 
                                 <div style="display: none;">
@@ -166,38 +166,38 @@
         <div class="block">
             <div class="block-content">
                 @if(count($categories) > 0)
-                @if ($categories)
-                    <table class="js-table-sections table table-hover table-borderless  table-vcenter">
-                        <thead>
-                        <tr>
-                            <th style="width: 30px;"></th>
-                            <th>Title</th>
-                            <th style="width: 15%;"></th>
-                            <th class="text-center" style="width: 15%;"></th>
-                        </tr>
-                        </thead>
-                        <?php $i = 1;?>
-                        @foreach($categories as $category)
+                    @if ($categories)
+                        <table class="js-table-sections table table-hover table-borderless  table-vcenter">
+                            <thead>
+                            <tr>
+                                <th style="width: 30px;"></th>
+                                <th>Title</th>
+                                <th style="width: 15%;"></th>
+                                <th class="text-center" style="width: 15%;"></th>
+                            </tr>
+                            </thead>
+                            <?php $i = 1;?>
+                            @foreach($categories as $category)
                                 <tbody class="js-table-sections-header " data-id="{{$category->id}}">
-                                    <tr>
-                                        <td class="text-center">
-                                            <i class="fa fa-angle-right"></i>
-                                        </td>
-                                        <td class="font-w600"> @if($category->icon != null) <img class="img-avatar img-avatar48" src="{{asset('categories-icons')}}/{{$category->icon}}" alt="" data-ranking="{{ $category->ranking }}"> @endif {{ $category->ranking }} ) {{ $category->title }}</td>
-                                        <td>
-                                            <span class="label label-primary"></span>
-                                        </td>
-                                        <td class="hidden-xs btn-group">
+                                <tr>
+                                    <td class="text-center">
+                                        <i class="fa fa-angle-right"></i>
+                                    </td>
+                                    <td class="font-w600"> @if($category->icon != null) <img class="img-avatar img-avatar48" src="{{asset('categories-icons')}}/{{$category->icon}}" alt="" data-ranking="{{ $category->ranking }}"> @endif {{ $category->ranking }} ) {{ $category->title }}</td>
+                                    <td>
+                                        <span class="label label-primary"></span>
+                                    </td>
+                                    <td class="hidden-xs btn-group">
 
-                                                <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
-                                                        data-target="#modal-popin{{$category->id}}" title="Edit Category"><i
-                                                        class="fa fa-edit"></i></button>
-                                                <a href="{{ route('category.delete', $category->id) }}"
-                                                   class="btn btn-sm btn-danger" type="button" data-toggle="tooltip" title=""
-                                                   data-original-title="Delete Category"><i class="fa fa-times"></i></a>
-                                        </td>
-                                    </tr>
-                                    <div class="modal fade" id="modal-popin{{$category->id}}" tabindex="-1" role="dialog"
+                                        <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
+                                                data-target="#modal-popin{{$category->id}}" title="Edit Category"><i
+                                                class="fa fa-edit"></i></button>
+                                        <a href="{{ route('category.delete', $category->id) }}"
+                                           class="btn btn-sm btn-danger" type="button" data-toggle="tooltip" title=""
+                                           data-original-title="Delete Category"><i class="fa fa-times"></i></a>
+                                    </td>
+                                </tr>
+                                <div class="modal fade" id="modal-popin{{$category->id}}" tabindex="-1" role="dialog"
                                      aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-popin">
                                         <div class="modal-content">
@@ -244,6 +244,7 @@
                                 <tbody>
                                 @if ($category->hasSub)
                                     @foreach($category->hasSub as $sub)
+                                        <tbody class="js-table-sections-header " data-id="{{$sub->id}}">
                                         <tr>
                                             <td class="text-center text-success">
                                             </td>
@@ -253,12 +254,12 @@
                                             </td>
                                             <td class="hidden-xs btn-group ">
 
-                                                    <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
-                                                            data-target="#sub{{$sub->id}}" title="Edit SubCategory"><i
-                                                            class="fa fa-edit"></i></button>
-                                                    <a href="{{ route('sub.delete', $sub->id) }}"
-                                                       class="btn btn-sm btn-danger" data-toggle="tooltip" title=""
-                                                       data-original-title="Delete SubCategory"><i class="fa fa-times"></i></a>
+                                                <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
+                                                        data-target="#sub{{$sub->id}}" title="Edit SubCategory"><i
+                                                        class="fa fa-edit"></i></button>
+                                                <a href="{{ route('sub.delete', $sub->id) }}"
+                                                   class="btn btn-sm btn-danger" data-toggle="tooltip" title=""
+                                                   data-original-title="Delete SubCategory"><i class="fa fa-times"></i></a>
 
                                             </td>
                                         </tr>
@@ -296,17 +297,78 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    @endforeach
-                                @endif
-                                </tbody>
-                        @endforeach
-                    </table>
-                @else
-                    <h3>no found</h3>
-                @endif
+                                        </tbody>
+                                        <tbody>
+                                        @if ($sub->hasSub)
+                                            @foreach($sub->hasSub as $inner)
+                                                <tbody class="js-table-sections-header " data-id="{{$inner->id}}">
+                                                <tr>
+                                                    <td class="text-center text-success">
+                                                    </td>
+                                                    <td class="font-w600 text-success">{{ $inner->title }}</td>
+                                                    <td>
+                                                        <small></small>
+                                                    </td>
+                                                    <td class="hidden-xs btn-group ">
+
+                                                        <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
+                                                                data-target="#sub{{$inner->id}}" title="Edit SubCategory"><i
+                                                                class="fa fa-edit"></i></button>
+                                                        <a href="{{ route('sub.delete', $inner->id) }}"
+                                                           class="btn btn-sm btn-danger" data-toggle="tooltip" title=""
+                                                           data-original-title="Delete SubCategory"><i class="fa fa-times"></i></a>
+
+                                                    </td>
+                                                </tr>
+                                                <div class="modal fade" id="sub{{$inner->id}}" tabindex="-1" role="dialog"
+                                                     aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-popin">
+                                                        <div class="modal-content">
+                                                            <div class="block block-themed block-transparent remove-margin-b">
+                                                                <div class="block-header bg-primary-dark">
+                                                                    <ul class="block-options">
+                                                                        <li>
+                                                                            <button data-dismiss="modal" type="button"><i
+                                                                                    class="si si-close"></i></button>
+                                                                        </li>
+                                                                    </ul>
+                                                                    <h3 class="block-title">Update SubCategory</h3>
+                                                                </div>
+                                                                <div class="block-content">
+                                                                    <form class="form-horizontal push-10-t"
+                                                                          action="{{ route('sub.update', $sub->id) }}"
+                                                                          method="post">
+                                                                        @csrf
+                                                                        <div class="form-group">
+                                                                            <input type="text" class="form-control" name="title"
+                                                                                   value="{{ $sub->title }}">
+                                                                        </div>
+                                                                        <div class="form-group text-right">
+                                                                            <button class="btn btn-sm btn-success " type="submit">
+                                                                                Update
+                                                                            </button>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </tbody>
+                                                @endforeach
+                                                @endif
+                                                </tbody>
+                                                @endforeach
+                                                @endif
+                                                </tbody>
+                                            @endforeach
+                        </table>
                     @else
-                <p>No Categories Created</p>
+                        <h3>no found</h3>
                     @endif
+                @else
+                    <p>No Categories Created</p>
+                @endif
             </div>
         </div>
     </div>
