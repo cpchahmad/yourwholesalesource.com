33d84aff05a9d0d65fced2ead5e77eb6183af8ae
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 17:57:23 +0500

    Working on Notifications

--- resources/views/layout/single_sidebar.blade.php
+++ resources/views/layout/single_sidebar.blade.php
@@ -222,7 +222,8 @@
 
             <div class="dropdown d-inline-block ml-2">
                 <button type="button" class="btn btn-sm btn-dual" id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="si si-bell"></i>
+                    Important Notifications
+                    <i class="si si-bell text-danger"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0 border-0 font-size-sm" aria-labelledby="page-header-notifications-dropdown">
                     <div class="p-2 bg-primary text-center">
