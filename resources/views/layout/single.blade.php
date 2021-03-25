b54696078cb4009209451c42327e6bd59d2c70f4
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 14:08:13 +0500

    Fixing Show more Button

--- resources/views/layout/single.blade.php
+++ resources/views/layout/single.blade.php
@@ -308,7 +308,7 @@
         <div class="content py-3">
             <div class="row font-size-sm">
                 <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-right">
-                    Designed by <i class="fa fa-bolt text-danger"></i> <a class="font-w600" href="https://tetralogicx.com" target="_blank">Fantasy Supply Limited</a>
+{{--                    Designed by <i class="fa fa-bolt text-danger"></i> <a class="font-w600" href="https://tetralogicx.com" target="_blank">Fantasy Supply Limited</a>--}}
                 </div>
             </div>
         </div>
