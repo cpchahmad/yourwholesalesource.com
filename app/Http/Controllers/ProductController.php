a3326fec81038073263211ec19fe2159a2bde719
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 13:03:31 +0500

    Editing Sub sub categories against products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -834,7 +834,6 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        dd($request->all());
         $product = Product::find($id);
         $woocommerce = $this->helper->getWooCommerceAdminShop();
 
