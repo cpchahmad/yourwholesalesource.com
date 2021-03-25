2328010b3d581856f223e509a7db25cbd19c6f60
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 12:14:54 +0500

    Showing Sub Categories on product create page

--- public/js/admin.js
+++ public/js/admin.js
@@ -325,6 +325,7 @@ $(document).ready(function () {
             var checked = $(this).parents('.product_sub_sub_cat').find('input[type=checkbox]:checked').length;
             if(checked === 0){
                 $(this).parents('.product_sub_sub_cat').prev().find('.sub_cat_checkbox').prop('checked',false);
+                $(this).parents('.product_sub_cat').prev().find('.category_checkbox').prop('checked',false);
             }
         }
     });
