1177250e33f4aca801bb393d2b7db1028c57c872
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 17:37:17 +0500

    Working on Tool Tip

--- resources/views/non_shopify_users/product/wefullfill_products.blade.php
+++ resources/views/non_shopify_users/product/wefullfill_products.blade.php
@@ -46,7 +46,7 @@
                 @if($index < 11)
                     <div class="col-md-3 p-0 tooltip-ex">
                         <span class="tooltip-ex-text bg-info ml-5">
-                            <a style="" href="https://fundraisingforacause.com/pages/ribbon-color-meanings" class="text-white" target="_blank">Click here to know what each ribbon means!</a>
+                            <a style="" href="https://fundraisingforacause.com/pages/ribbon-color-meanings" class="text-white" target="_blank">Ribbon meaning?</a>
                         </span>
                         <a href="{{route('users.product.wefulfill')}}?category={{$category->title}}">
                             <div class="block m-0">
