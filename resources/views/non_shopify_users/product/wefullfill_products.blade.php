5db36def267911559d4a09ed28017a1ab1da7afc
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-25 17:33:43 +0500

    Working on Tool Tip

--- resources/views/non_shopify_users/product/wefullfill_products.blade.php
+++ resources/views/non_shopify_users/product/wefullfill_products.blade.php
@@ -46,7 +46,7 @@
                 @if($index < 11)
                     <div class="col-md-3 p-0 tooltip-ex">
                         <span class="tooltip-ex-text bg-info ml-5">
-                            <a style="" href="https://fundraisingforacause.com/pages/ribbon-color-meanings" class="text-white" target="_blank">Click Here</a>
+                            <a style="" href="https://fundraisingforacause.com/pages/ribbon-color-meanings" class="text-white" target="_blank">Click here to know what each ribbon means!</a>
                         </span>
                         <a href="{{route('users.product.wefulfill')}}?category={{$category->title}}">
                             <div class="block m-0">
@@ -59,7 +59,7 @@
                 @endif
                 @if($index == 11)
                     <div class="col-md-3 p-0 see-more-block mt-4">
-                        <div class="block pointer  m-0">
+                        <div class="block  m-0">
                             <div class="block-content p-3 text-center">
                                 <p  class="m-0" style="font-size:14px;">See More ....</p>
                             </div>
@@ -68,7 +68,10 @@
                 @endif
 
                 @if($index >= 11)
-                    <div class="col-md-3 p-0 after12" style="display: none" >
+                    <div class="col-md-3 p-0 after12 tooltip-ex" style="display: none" >
+                        <span class="tooltip-ex-text bg-info ml-5">
+                            <a style="" href="https://fundraisingforacause.com/pages/ribbon-color-meanings" class="text-white" target="_blank">Click here to know what each ribbon means!</a>
+                        </span>
                         <a href="{{route('users.product.wefulfill')}}?category={{$category->title}}" data-toggle="tooltip" title=""
                            data-original-title="Delete Order">
                             <div class="block pointer m-0">
