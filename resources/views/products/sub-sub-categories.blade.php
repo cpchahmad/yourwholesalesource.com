2b7daad377659b0d6bd38715458f9d689a8bba8b
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-26 15:51:03 +0500

    Adding Sub Sub Category filter to products page

--- resources/views/products/sub-sub-categories.blade.php
+++ resources/views/products/sub-sub-categories.blade.php
@@ -1,4 +1,4 @@
-<select name="child_category" id="" class="form-control sub-sub-category">
+<select name="sub_sub_category" id="" class="form-control sub-sub-category">
     <option value="" style="display: none">Sub Sub Categories</option>
     @foreach($sub_sub_categories as $category)
         <option value="{{ $category->title }}" >{{ $category->title }}</option>
