83ebcdb12a84605b24045a12653a166aeb62d807
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 14:38:26 +0500

    Storing Ribbons and showing to admin

--- resources/views/setttings/videos/index.blade.php
+++ resources/views/setttings/videos/index.blade.php
@@ -244,99 +244,99 @@
             <div class="block-header font-size-h5 font-w600 pl-0">
                 Ribbons
             </div>
-            <div class="table-responsive">
-                <table class="table table-borderless table-striped table-vcenter">
-                    <thead>
-                    <tr>
-                        <th>Color</th>
-                        <th>Cause</th>
-                        <th>Created At</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @if(count($ribbons))
-                            @foreach($ribbons as $index => $ribbon)
+            @if(count($ribbons))
+                <div class="table-responsive">
+                    <table class="table table-borderless table-striped table-vcenter">
+                        <thead>
                         <tr>
-                            <td class="font-w600" style="vertical-align: middle">
-                                {{ $ribbon->color }}
-                            </td>
-                            <td style="vertical-align: middle">
-                                {{ $ribbon->cause }}
-                            </td>
-                            <td style="vertical-align: middle">
-                                {{ date_format($ribbon->created_at ,"Y/M/d H:i ") }}
-                            </td>
-                            <td class="text-right btn-group" style="float: right">
-                                <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
-                                        data-target="#edit_video_modal{{$ribbon->id}}"><i
-                                        class="fa fa-edit"></i>
-                                </button>
-                                <form method="POST" action="{{ route('admin.videos.delete', $ribbon->id) }}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-sm btn-danger" type="button" data-toggle="tooltip" title=""
-                                            data-original-title="Delete Video"><i class="fa fa-times"></i></button>
-                                </form>
-                            </td>
-                            <div class="modal fade" id="edit_video_modal{{$ribbon->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-block-popout" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-popout" role="document">
-                                    <div class="modal-content">
-                                        <div class="block block-themed block-transparent mb-0">
-                                            <div class="block-header bg-primary-dark">
-                                                <h3 class="block-title">Edit "{{ $ribbon->title}}"</h3>
-                                                <div class="block-options">
-                                                    <button type="button" class="btn-block-option">
-                                                        <i class="fa fa-fw fa-times"  data-dismiss="modal" aria-label="Close"></i>
-                                                    </button>
+                            <th>Color</th>
+                            <th>Cause</th>
+                            <th>Created At</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                             @foreach($ribbons as $index => $ribbon)
+                            <tr>
+                                <td class="font-w600" style="vertical-align: middle">
+                                    {{ $ribbon->color }}
+                                </td>
+                                <td style="vertical-align: middle">
+                                    {{ $ribbon->cause }}
+                                </td>
+                                <td style="vertical-align: middle">
+                                    {{ date_format($ribbon->created_at ,"Y/M/d H:i ") }}
+                                </td>
+                                <td class="text-right btn-group" style="float: right">
+                                    <button class="btn btn-sm btn-warning" type="button" data-toggle="modal"
+                                            data-target="#edit_video_modal{{$ribbon->id}}"><i
+                                            class="fa fa-edit"></i>
+                                    </button>
+                                    <form method="POST" action="{{ route('admin.videos.delete', $ribbon->id) }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-danger" type="button" data-toggle="tooltip" title=""
+                                                data-original-title="Delete Video"><i class="fa fa-times"></i></button>
+                                    </form>
+                                </td>
+                                <div class="modal fade" id="edit_video_modal{{$ribbon->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-block-popout" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-popout" role="document">
+                                        <div class="modal-content">
+                                            <div class="block block-themed block-transparent mb-0">
+                                                <div class="block-header bg-primary-dark">
+                                                    <h3 class="block-title">Edit "{{ $ribbon->title}}"</h3>
+                                                    <div class="block-options">
+                                                        <button type="button" class="btn-block-option">
+                                                            <i class="fa fa-fw fa-times"  data-dismiss="modal" aria-label="Close"></i>
+                                                        </button>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <form action="{{route('admin.videos.edit',$ribbon->id)}}" method="post">
-                                                @csrf
-                                                <div class="block-content font-size-sm">
-                                                    <div class="form-group">
-                                                        <div class="col-sm-12">
-                                                            <div class="form-group">
-                                                                <label for="material-error">Ribbon Color</label>
-                                                                <input  class="form-control  @error('color') is-invalid @enderror" type="text" id="zone_title"  value="{{ $ribbon->color }}" name="title" placeholder="Enter ribbon color..">
-                                                                @error('color')
-                                                                <div class="invalid-feedback">{{ $message }}</div>
-                                                                @enderror
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <label for="material-error">Cause</label>
-                                                                <input  class="form-control  @error('cause') is-invalid @enderror" type="text" id="zone_title" {{ $ribbon->cause }}  name="link" placeholder="Enter ribbon color cause..">
-                                                                @error('cause')
-                                                                <div class="invalid-feedback">{{ $message }}</div>
-                                                                @enderror
-                                                            </div>
-                                                            <div class="form-group">
-                                                                <label for="material-error">Ribbon Image</label>
-                                                                <input  class="form-control  @error('image') is-invalid @enderror" type="file" id="zone_title"  name="link" placeholder="Select ribbon image..">
-                                                                @error('image')
-                                                                <div class="invalid-feedback">{{ $message }}</div>
-                                                                @enderror
+                                                <form action="{{route('admin.videos.edit',$ribbon->id)}}" method="post">
+                                                    @csrf
+                                                    <div class="block-content font-size-sm">
+                                                        <div class="form-group">
+                                                            <div class="col-sm-12">
+                                                                <div class="form-group">
+                                                                    <label for="material-error">Ribbon Color</label>
+                                                                    <input  class="form-control  @error('color') is-invalid @enderror" type="text" id="zone_title"  value="{{ $ribbon->color }}" name="title" placeholder="Enter ribbon color..">
+                                                                    @error('color')
+                                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                                    @enderror
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="material-error">Cause</label>
+                                                                    <input  class="form-control  @error('cause') is-invalid @enderror" type="text" id="zone_title" {{ $ribbon->cause }}  name="link" placeholder="Enter ribbon color cause..">
+                                                                    @error('cause')
+                                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                                    @enderror
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label for="material-error">Ribbon Image</label>
+                                                                    <input  class="form-control  @error('image') is-invalid @enderror" type="file" id="zone_title"  name="link" placeholder="Select ribbon image..">
+                                                                    @error('image')
+                                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                                    @enderror
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
 
-                                                <div class="block-content block-content-full text-right border-top">
-                                                    <button type="submit" class="btn btn-sm btn-primary" >Save</button>
-                                                </div>
-                                            </form>
+                                                    <div class="block-content block-content-full text-right border-top">
+                                                        <button type="submit" class="btn btn-sm btn-primary" >Save</button>
+                                                    </div>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                        </tr>
-                    @endforeach
-                        @else
-                            <div class="text-center p-3">No Ribbons Added Yet!</div>
-                        @endif
-                    </tbody>
-                </table>
-            </div>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <div class="text-center p-3">No Ribbons Added Yet!</div>
+            @endif
         </div>
     </div>
 
