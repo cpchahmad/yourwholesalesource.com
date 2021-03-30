8ec7c0f8206193bb39bab0d5c24b163332ab8f82
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 12:52:51 +0500

    Showing Handling Fee on Admin side user side

--- resources/views/single-store/orders/view.blade.php
+++ resources/views/single-store/orders/view.blade.php
@@ -502,7 +502,7 @@
                                     <td></td>
                                     <td align="right">
                                         @if($order->paid == 0)
-    {{--                                        <button class="btn btn-success" data-toggle="modal" data-target="#payment_modal"><i class="fa fa-credit-card"></i> Credit Card Pay</button>--}}
+                                            <button class="btn btn-success" data-toggle="modal" data-target="#payment_modal"><i class="fa fa-credit-card"></i> Credit Card Pay</button>
                                             <button class="btn btn-success paypal-pay-button" data-toggle="modal" data-target="#paypal_pay_trigger" data-href="{{route('store.order.paypal.pay',$order->id)}}" data-percentage="{{$settings->paypal_percentage}}" data-fee="{{number_format($order->cost_to_pay  *$settings->paypal_percentage/100,2)}}" data-subtotal="{{number_format($order->cost_to_pay,2)}}" data-pay=" {{number_format($order->cost_to_pay+($order->cost_to_pay*$settings->paypal_percentage/100),2)}} USD" ><i class="fab fa-paypal"></i> Paypal Pay</button>
                                             <button class="btn btn-success wallet-pay-button" data-href="{{route('store.order.wallet.pay',$order->id)}}" data-pay=" {{ number_format($order->total_cost + $order->shipping_rate + $order->handling_fee , 2) }}" ><i class="fa fa-wallet"></i> Wallet Pay</button>
 
