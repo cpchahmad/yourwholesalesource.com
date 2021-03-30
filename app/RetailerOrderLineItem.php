c3a8c2071f3c205e45f22af2aca38da7fc4e5120
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 19:33:02 +0500

    Adding Users to Omnilist after regsitration

--- app/RetailerOrderLineItem.php
+++ app/RetailerOrderLineItem.php
@@ -89,7 +89,7 @@ class RetailerOrderLineItem extends Model
         elseif($this->linked_admin_product)
             $weight = $this->linked_admin_product->weight *  $this->quantity;
         elseif($this->linked_product != null && $this->linked_product->linked_product)
-            $weight = $this->linked_product->linked_product->weight *  $this->quantity;
+            $weight = $this->linked_product->weight *  $this->quantity;
 
         return $weight;
 
