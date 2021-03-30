9ef8f79868642d323c345bf7f2f6dbf8086da6c9
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 18:04:40 +0500

    Adding Users to Omnilist after regsitration

--- app/Listeners/SendEmailsAfterRegistration.php
+++ app/Listeners/SendEmailsAfterRegistration.php
@@ -54,46 +54,22 @@ class SendEmailsAfterRegistration
             'createdAt' => $user->created_at,
             'firstName' => $user->name,
             'tags' => [
-                0 => 'Our Memebers',
+                0 => 'designer'
             ],
             'identifiers' => [
                 0 => [
                     'type' => 'email',
-                    'id' => 'austin.powers@example.com',
+                    'id' => $user->email,
                     'channels' => [
                         'email' => [
                             'status' => 'subscribed',
                             'statusDate' => '2016-02-29T10:07:28Z',
                         ],
                     ],
-                ],
-                1 => [
-                    'type' => 'phone',
-                    'id' => '+443031237300',
-                    'channels' => [
-                        'sms' => [
-                            'status' => 'nonSubscribed',
-                            'statusDate' => '2016-02-29T10:07:28Z',
-                        ],
-                    ],
-                ],
-            ],
-            'country' => 'United Kingdom',
-            'countryCode' => 'GB',
-            'state' => '',
-            'city' => 'London',
-            'address' => 'Westminster',
-            'postalCode' => 'SW1A 1AA',
-            'gender' => 'f',
-            'birthdate' => '1997-05-02',
-            'customProperties' => [
-                'age' => 33,
-                'hair_color' => 'brown',
-                'married' => true,
-                'marriageDate' => '2018-07-07',
-                'loyaltyPoints' => 125.8,
-            ],
+                ]
+            ]
         ];
+
 
         curl_setopt_array($curl, array(
             CURLOPT_URL => "https://api.omnisend.com/v3/contacts",
@@ -103,10 +79,10 @@ class SendEmailsAfterRegistration
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{\"createdAt\":\"2016-05-02T09:19:19Z\",\"firstName\":\"Vanessa\",\"lastName\":\"Kensington\",\"tags\":[\"designer\",\"leader\",\"source: shopify\"],\"identifiers\":[{\"type\":\"email\",\"id\":\"austin.powers@example.com\",\"channels\":{\"email\":{\"status\":\"subscribed\",\"statusDate\":\"2016-02-29T10:07:28Z\"}}},{\"type\":\"phone\",\"id\":\"+443031237300\",\"channels\":{\"sms\":{\"status\":\"nonSubscribed\",\"statusDate\":\"2016-02-29T10:07:28Z\"}}}],\"country\":\"United Kingdom\",\"countryCode\":\"GB\",\"state\":\"\",\"city\":\"London\",\"address\":\"Westminster\",\"postalCode\":\"SW1A 1AA\",\"gender\":\"f\",\"birthdate\":\"1997-05-02\",\"customProperties\":{\"age\":33,\"hair_color\":\"brown\",\"married\":true,\"marriageDate\":\"2018-07-07\",\"loyaltyPoints\":125.8}}",
+            CURLOPT_POSTFIELDS => json_encode($data),
             CURLOPT_HTTPHEADER => array(
                 "content-type: application/json",
-                "x-api-key: 606318754c7fa4545433398e-On0aNj7aqEIeOjWkFv3uUrvP6SNRlw6vWmtL6pdbsghhzFPZjq"
+                "x-api-key: ". env('OMNISEND_API_KEY')
             ),
         ));
 
@@ -115,11 +91,8 @@ class SendEmailsAfterRegistration
 
         curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
-
+        $log = new ErrorLog();
+        $log->message(json_encode($response));
+        $log->save();
     }
 }
