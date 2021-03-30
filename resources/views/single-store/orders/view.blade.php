d9f4f821e6f8cf508666e5f2247b80a89ecddc02
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 12:41:52 +0500

    Showing Handling Fee on Admin side user side

--- resources/views/single-store/orders/view.blade.php
+++ resources/views/single-store/orders/view.blade.php
@@ -517,9 +517,9 @@
                                                                         Subtotal: {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ),2)}} USD
                                                                         <br>
                                                                         WholeSaleSource Paypal Fee ({{$settings->paypal_percentage}}%): {{number_format($order->total_cost + $order->shipping_rate + $order->handling_fee *$settings->paypal_percentage/100,2)}} USD
-                                                                        <br>Total Cost : {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee )+(($order->total_cost + $order->shipping_rate + $order->handling_fee)*$settings->paypal_percentage/100),2)}} USD</p>
+                                                                        <br>Total Cost : {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee )+($order->total_cost + $order->shipping_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}} USD</p>
                                                                 </div>
-                                                                <p> A amount of  {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ) +(($order->total_cost + $order->shipping_rate + $order->handling_fee)*$settings->paypal_percentage/100),2)}} USD will be deducted through your Paypal Account</p>
+                                                                <p> A amount of  {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ) +($order->total_cost + $order->shipping_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}} USD will be deducted through your Paypal Account</p>
 
                                                                 <div class="paypal_btn_trigger">
                                                                     <div class="paypal-button-container"></div>
