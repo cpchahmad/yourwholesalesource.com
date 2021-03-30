f4041322ecbb6cbaf28f4a648780b161eda4b078
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 12:16:06 +0500

    Showing Handling Fee on Non-shopify user side

--- resources/views/non_shopify_users/orders/view.blade.php
+++ resources/views/non_shopify_users/orders/view.blade.php
@@ -573,7 +573,7 @@
                                                                     WholeSaleSource Paypal Fee ({{$settings->paypal_percentage}}%): {{number_format($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee  *$settings->paypal_percentage/100,2)}} USD
                                                                     <br>Total Cost : {{number_format(($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee ) +(($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee )*$settings->paypal_percentage/100),2)}} USD</p>
                                                             </div>
-                                                            <p> A amount of  {{number_format(($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee) + (($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee )*$settings->paypal_percentage/100),2)}} USD will be deducted through your Paypal Account</p>
+                                                            <p> A amount of  {{ number_format($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee,2) + number_format(($order->total_cost + $order->shipping_rate_for_non_shopify + $order->handling_fee )*$settings->paypal_percentage/100,2) }} USD will be deducted through your Paypal Account</p>
 
                                                             <div class="paypal_btn_trigger">
                                                                 <div id="paypal-button-container"></div>
