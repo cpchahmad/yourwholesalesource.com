5234ab167482426e20acb962886d2345153061e8
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-30 17:50:47 +0500

    Adding Users to Omnilist after regsitration

--- app/Listeners/SendEmailsAfterRegistration.php
+++ app/Listeners/SendEmailsAfterRegistration.php
@@ -34,45 +34,56 @@ class SendEmailsAfterRegistration
     {
         $user = $event->user;
 
-        try{
-            Mail::to($user->email)->send(new NewUser($user));
-            Mail::to($user->email)->send(new NewWallet($user));
-            Mail::to($user->email)->send(new TopShopifyProuctMail($user));
-            Mail::to($user->email)->send(new StoreIntegrationMail());
-            Mail::to($user->email)->send(new ResourcesMail());
-        }
-        catch (\Exception $e){
-            $log = new ErrorLog();
-            $log->message = $e->getMessage();
-            $log->save();
-        }
+//        try{
+//            Mail::to($user->email)->send(new NewUser($user));
+//            Mail::to($user->email)->send(new NewWallet($user));
+//            Mail::to($user->email)->send(new TopShopifyProuctMail($user));
+//            Mail::to($user->email)->send(new StoreIntegrationMail());
+//            Mail::to($user->email)->send(new ResourcesMail());
+//        }
+//        catch (\Exception $e){
+//            $log = new ErrorLog();
+//            $log->message = $e->getMessage();
+//            $log->save();
+//        }
 
-        // Sync To SendGrid WefullFill Members Contact List
-        $contacts = [];
-        array_push($contacts, [
-            'email' => $user->email,
-            'first_name' => $user->name,
-        ]);
-        $contacts_payload = [
-            'list_ids' => ["33d743f3-a906-4512-83cd-001f7ba5ab33"],
-            'contacts' => $contacts
-        ];
-        $payload = json_encode($contacts_payload);
-
+        // Sync To Omnisend Members Contact List
         $curl = curl_init();
 
+        $data = [
+            'createdAt' => $user->created_at,
+            'firstName' => $user->name,
+            'sendWelcomeMessage' => true,
+            'tags' => [
+                0 => 'Our Members',
+            ],
+            'identifiers' => [
+                0 => [
+                    'type' => 'email',
+                    'id' => $user->email,
+                    'channels' => [
+                        'email' => [
+                            'status' => 'subscribed',
+                            'statusDate' => $user->created_at,
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.sendgrid.com/v3/marketing/contacts",
+            CURLOPT_URL => "https://api.omnisend.com/v3/contacts",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "PUT",
-            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($data),
             CURLOPT_HTTPHEADER => array(
-                "authorization: Bearer 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",
-                "content-type: application/json"
+                "content-type: application/json",
+                "x-api-key: ". env('OMNISEND_API_KEY')
             ),
         ));
 
@@ -80,5 +91,12 @@ class SendEmailsAfterRegistration
         $err = curl_error($curl);
 
         curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+
     }
 }
