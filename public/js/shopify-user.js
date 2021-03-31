f23a05a737e0f2b118dc9e5e026e86628de3d001
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-31 15:11:28 +0500

    Adding Stripe Payment to non-shopify user

--- public/js/shopify-user.js
+++ public/js/shopify-user.js
@@ -1,5 +1,39 @@
 $(document).ready(function () {
 
+    // Stripe
+    var $form = $(".require-validation");
+
+    $('form.require-validation').bind('submit', function(e) {
+        var $form = $(".require-validation"),
+            inputSelector = ['input[type=email]', 'input[type=password]',
+                'input[type=text]', 'input[type=file]',
+                'textarea'
+            ].join(', '),
+            $inputs = $form.find('.required').find(inputSelector),
+            $errorMessage = $form.find('div.error'),
+            valid = true;
+        $errorMessage.addClass('hide');
+        $('.has-error').removeClass('has-error');
+        $inputs.each(function(i, el) {
+            var $input = $(el);
+            if ($input.val() === '') {
+                $input.parent().addClass('has-error');
+                $errorMessage.removeClass('hide');
+                e.preventDefault();
+            }
+        });
+        if (!$form.data('cc-on-file')) {
+            e.preventDefault();
+            Stripe.setPublishableKey($form.data('stripe-publishable-key'));
+            Stripe.createToken({
+                number: $('.card-number').val(),
+                cvc: $('.card-cvc').val(),
+                exp_month: $('.card-expiry-month').val(),
+                exp_year: $('.card-expiry-year').val()
+            }, stripeResponseHandler);
+        }
+    });
+
     $('body').on('click','.see-more-block',function () {
         $('.after12').show();
         $(this).hide();
