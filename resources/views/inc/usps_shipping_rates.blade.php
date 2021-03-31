aa3c55ee62296717f275d456e95fb1b61262a101
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-31 12:13:03 +0500

    Showing Usps Rates on order page

--- resources/views/inc/usps_shipping_rates.blade.php
+++ resources/views/inc/usps_shipping_rates.blade.php
@@ -1,5 +1,5 @@
 <select name="" id="" class="form-control">
     @foreach($rates as $rate)
-        <option value="{{ $rate->Postage->Rate }}">{{ $rate->Postage->MailService . ': '. $rate->Postage->Rate .' USD' }}</option>
+        <option value="{{ $rate->Postage->Rate }}">{{ $rate->Postage->Rate .' USD' }}</option>
     @endforeach
 </select>
