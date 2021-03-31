cddffee66409d94d5c973012dc49c36463675f4a
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-31 20:24:47 +0500

    Adding Wallet payment by paypal

--- resources/views/setttings/wallets/wallet_detail.blade.php
+++ resources/views/setttings/wallets/wallet_detail.blade.php
@@ -306,6 +306,147 @@
                         </div>
                     </div>
 
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="block">
+                                <div class="block-header">
+                                    <div class="block-title">
+                                        Paypal Top-up Requests
+                                    </div>
+                                </div>
+                                <div class="block-content">
+                                    @if (count($wallet->requests()->where('type','paypal')->get()) > 0)
+                                        <table class="table table-hover table-borderless table-striped table-vcenter">
+                                            <thead>
+                                            <tr>
+                                                <th>Company/Sender Title</th>
+                                                <th>Amount</th>
+                                                <th>Notes</th>
+                                                <th>Status</th>
+                                                <th></th>
+                                            </tr>
+                                            </thead>
+
+                                            @foreach($wallet->requests()->where('type','paypal')->get() as $index => $req)
+                                                <tbody class="">
+                                                <tr>
+
+                                                    <td>
+                                                        {{$req->cheque_title}}
+                                                    </td>
+                                                    <td>
+                                                        {{number_format($req->amount,2)}} USD
+                                                    </td>
+                                                    <td>
+                                                        @if($req->notes != null)
+                                                            {{$req->notes}}
+                                                        @else
+                                                            No Notes
+                                                        @endif
+                                                    </td>
+
+                                                    <td>
+                                                        @if($req->status == 0)
+                                                            <span class="badge badge-warning">Pending</span>
+                                                        @else
+                                                            <span class="badge badge-success">Approved</span>
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                    <td class="text-right">
+
+                                                        @if($req->status == 0)
+                                                            <button type="button" data-toggle="modal" data-target="#edit_bank_approve_modal{{$req->id}}" class="btn btn-sm btn-primary">Edit</button>
+                                                            <button class="btn btn-sm btn-success <!--approve-bank-transfer-button-->" data-toggle="modal" data-target="#bank_approve_modal{{$req->id}}" {{--data-route="{{route('sales_managers.wallets.approve.request',$req->id)}}" data-wallet="{{$wallet->wallet_token}}" data-amount="{{number_format($req->amount,2)}} USD" --}} > Approve</button>
+                                                        @endif
+
+                                                    </td>
+
+                                                </tr>
+                                                </tbody>
+                                                <div class="modal fade" id="edit_bank_approve_modal{{$req->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-block-popout" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-popout" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="block block-themed block-transparent mb-0">
+                                                                <div class="block-header bg-primary-dark">
+                                                                    <h3 class="block-title">Edit Top-up</h3>
+                                                                    <div class="block-options">
+                                                                        <button type="button" class="btn-block-option">
+                                                                            <i class="fa fa-fw fa-times"  data-dismiss="modal" aria-label="Close"></i>
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                                <form action="{{route('admin.wallets.edit',$req->id)}}" method="get">
+                                                                    <div class="block-content font-size-sm">
+                                                                        <div class="form-group">
+                                                                            <div class="form-material">
+                                                                                <label for="material-error">Company/Sender Name</label>
+                                                                                <input required class="form-control" type="text"  name="cheque_title"
+                                                                                       value="{{ $req->cheque_title }}" >
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <div class="form-material">
+                                                                                <label for="material-error">Amount</label>
+                                                                                <input required class="form-control" type="text"  name="amount"
+                                                                                       value="{{ $req->amount }}" >
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="block-content block-content-full text-right border-top">
+                                                                        <button type="submit" class="btn btn-sm btn-primary" >Edit Wallet Request</button>
+                                                                    </div>
+                                                                </form>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="modal fade" id="bank_approve_modal{{$req->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-block-popout" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-popout" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="block block-themed block-transparent mb-0">
+                                                                <div class="block-header bg-primary-dark">
+                                                                    <h3 class="block-title">Approve Top-up</h3>
+                                                                    <div class="block-options">
+                                                                        <button type="button" class="btn-block-option">
+                                                                            <i class="fa fa-fw fa-times"  data-dismiss="modal" aria-label="Close"></i>
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                                <form action="{{route('admin.wallets.approve.request',$req->id)}}" method="get">
+                                                                    <div class="block-content font-size-sm">
+                                                                        <div class="form-group">
+                                                                            <div class="col-sm-12">
+                                                                                <div class="form-material">
+                                                                                    <label for="material-error">Approved Date</label>
+                                                                                    <input required class="form-control" type="date"  name="date"
+                                                                                           value="" >
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="block-content block-content-full text-right border-top">
+                                                                        <button type="submit" class="btn btn-sm btn-primary" >Approved</button>
+                                                                    </div>
+                                                                </form>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                            @endforeach
+                                        </table>
+                                    @else
+                                        <p>No  Bank Transfer Requests Found</p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                   @include('inc.wallet_log')
                 </div>
             </div>
