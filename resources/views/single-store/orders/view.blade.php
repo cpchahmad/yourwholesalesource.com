b69a6e6c05766b72f8194a5b57ba62999993f181
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-03-31 13:14:19 +0500

    Showing Usps Rates on order page

--- resources/views/single-store/orders/view.blade.php
+++ resources/views/single-store/orders/view.blade.php
@@ -483,7 +483,7 @@
                                         @endphp
 {{--                                        {{ shipping_rate }} USD--}}
 {{--                                        {{number_format($order->shipping_price,2)}} USD--}}
-                                        {{ $usps_rate == 0 ? 'The Address is not Valid' : $usps_rate }}
+                                        {{ $usps_rate == 0 ? 'The Address is not Valid' : $usps_rate . 'USD'}}
                                      </td>
                                 </tr>
 
@@ -501,7 +501,7 @@
                                         Total Cost @if($order->paid == 0) to Pay @endif
                                     </td>
                                     <td align="right" class="total">
-                                        {{number_format($order->total_cost + $order->shipping_rate + $order->handling_fee ,2)}} USD
+                                        {{number_format($order->total_cost + $usps_rate + $order->handling_fee ,2)}} USD
                                     </td>
                                 </tr>
                                 <tr>
@@ -510,7 +510,7 @@
                                         @if($order->paid == 0)
     {{--                                        <button class="btn btn-success" data-toggle="modal" data-target="#payment_modal"><i class="fa fa-credit-card"></i> Credit Card Pay</button>--}}
                                             <button class="btn btn-success paypal-pay-button" data-toggle="modal" data-target="#paypal_pay_trigger" data-href="{{route('store.order.paypal.pay',$order->id)}}" data-percentage="{{$settings->paypal_percentage}}" data-fee="{{number_format($order->cost_to_pay  *$settings->paypal_percentage/100,2)}}" data-subtotal="{{number_format($order->cost_to_pay,2)}}" data-pay=" {{number_format($order->cost_to_pay+($order->cost_to_pay*$settings->paypal_percentage/100),2)}} USD" ><i class="fab fa-paypal"></i> Paypal Pay</button>
-                                            <button class="btn btn-success wallet-pay-button" data-href="{{route('store.order.wallet.pay',$order->id)}}" data-pay=" {{ number_format($order->total_cost + $order->shipping_rate + $order->handling_fee , 2) }}" ><i class="fa fa-wallet"></i> Wallet Pay</button>
+                                            <button class="btn btn-success wallet-pay-button" data-href="{{route('store.order.wallet.pay',$order->id)}}" data-pay=" {{ number_format($order->total_cost + $usps_rate + $order->handling_fee , 2) }}" ><i class="fa fa-wallet"></i> Wallet Pay</button>
 
 
                                             <div class="modal" id="paypal_pay_trigger" tabindex="-1" role="dialog" aria-labelledby="modal-block-vcenter" aria-hidden="true">
@@ -520,12 +520,12 @@
                                                             <div class="block-content cst_content_wrapper font-size-sm text-center">
                                                                 <h2>Are your sure?</h2>
                                                                 <div class="text-center"> <p>
-                                                                        Subtotal: {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ),2)}} USD
+                                                                        Subtotal: {{number_format(($order->total_cost + $usps_rate + $order->handling_fee ),2)}} USD
                                                                         <br>
-                                                                        YourWholesaleSource Paypal Fee ({{$settings->paypal_percentage}}%): {{number_format($order->total_cost + $order->shipping_rate + $order->handling_fee *$settings->paypal_percentage/100,2)}} USD
-                                                                        <br>Total Cost : {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee )+($order->total_cost + $order->shipping_rate + $order->handling_fee *$settings->paypal_percentage/100),2)}} USD</p>
+                                                                        YourWholesaleSource Paypal Fee ({{$settings->paypal_percentage}}%): {{number_format($order->total_cost + $usps_rate + $order->handling_fee *$settings->paypal_percentage/100,2)}} USD
+                                                                        <br>Total Cost : {{number_format(($order->total_cost + $usps_rate + $order->handling_fee )+($order->total_cost + $usps_rate + $order->handling_fee *$settings->paypal_percentage/100),2)}} USD</p>
                                                                 </div>
-                                                                <p> A amount of  {{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ) +($order->total_cost + $order->shipping_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}} USD will be deducted through your Paypal Account</p>
+                                                                <p> A amount of  {{number_format(($order->total_cost + $usps_rate + $order->handling_fee ) +($order->total_cost + $usps_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}} USD will be deducted through your Paypal Account</p>
 
                                                                 <div class="paypal_btn_trigger">
                                                                     <div class="paypal-button-container"></div>
@@ -556,7 +556,7 @@
                                                         return actions.order.create({
                                                             purchase_units: [{
                                                                 amount: {
-                                                                    value: '{{number_format(($order->total_cost + $order->shipping_rate + $order->handling_fee ) +($order->total_cost + $order->shipping_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}}'
+                                                                    value: '{{number_format(($order->total_cost + $usps_rate + $order->handling_fee ) +($order->total_cost + $order->shipping_rate + $order->handling_fee*$settings->paypal_percentage/100),2)}}'
                                                                 }
                                                             }]
                                                         });
