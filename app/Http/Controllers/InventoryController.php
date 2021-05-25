faa7aa7e75d5fb124bd212a7df8b09018664d720
yasir-naseer-tetralogicx <yasir@tetralogicx.com>
2021-05-25 13:45:24 +0500

    Syncing Inflow Product from client inflow

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -229,7 +229,7 @@ class InventoryController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://cloudapi.inflowinventory.com/c56df956-12ae-42f6-a237-0f184b484d87/products?include=inventoryLines?count=5',
+            CURLOPT_URL => 'https://cloudapi.inflowinventory.com/c56df956-12ae-42f6-a237-0f184b484d87/products?include=inventoryLines&count=500',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
