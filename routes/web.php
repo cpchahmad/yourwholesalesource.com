bd3dfdac9b472b0816e73edc0503fe5afa84ca48
Tetralogicx Pvt Ltd <yasir@tetralogicx.com>
2021-06-11 11:29:45 +0500

    Adding Shop Login

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,9 @@ use Illuminate\Support\Facades\Redirect;
 
 Route::get('/order/download/{id}/csv', 'CustomOrderController@download_order')->name('app.order.download');
 
-Route::get('/shop-login', function () {
-
-    return \redirect('/sdf');
+Route::get('/shop-login', function (\Illuminate\Http\Request  $request) {
+    dd($request->all());
+    return \redirect('/authenticate?shop');
 });
 
 
