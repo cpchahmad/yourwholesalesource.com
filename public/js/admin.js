a2c24aba738512c1593fd7d6b065b594bd33fac6
Tetralogicx Pvt Ltd <yasir@tetralogicx.com>
2021-06-11 12:50:45 +0500

    checking issue with category selector

--- public/js/admin.js
+++ public/js/admin.js
@@ -320,7 +320,7 @@ $(document).ready(function () {
     $('body').on('change','.sub_cat_checkbox',function () {
         console.log('sub');
         if($(this).is(':checked')){
-            console.log($(this).closest('.product_sub_cat'));
+            console.log($(this).parent('.product_sub_cat'));
             $(this).parent('.product_sub_cat').prev().find('.category_checkbox').prop('checked',true);
 
         }
