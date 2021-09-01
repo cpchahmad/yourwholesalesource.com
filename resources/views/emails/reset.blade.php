26d31e55ebcb29d1e87e75a92d487d8948e90e0f
Tetralogicx Pvt Ltd <yasir@tetralogicx.com>
2021-09-01 15:09:59 +0500

    Changing name

--- resources/views/emails/reset.blade.php
+++ resources/views/emails/reset.blade.php
@@ -360,7 +360,7 @@
 
                                                             <!-- TABLE LEFT -->
 
-                                                            <table align="center" border="0" cellpadding="0" cellspacing="0" class="display-width" width="100%" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
+                                                            <table align="center" border="0" cellpadding="0" cellspacing="0" class="display-width" width="43%" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;">
                                                                 <tbody>
                                                                 <tr>
                                                                     <td align="center">
