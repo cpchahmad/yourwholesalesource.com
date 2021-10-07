a754339aa323370b19b731548c46c8015d2fa9e5
Tetralogicx Pvt Ltd <yasir@tetralogicx.com>
2021-10-07 12:40:52 +0500

    Adding Store auth logic

--- resources/views/single-store/index.blade.php
+++ resources/views/single-store/index.blade.php
@@ -126,46 +126,60 @@
                                 <div class="block-header">
                                     <h3 class="block-title">Associate with an account</h3>
                                 </div>
-                                <form id="authenticate_user_form" data-store="{{$shop->shopify_domain}}" data-token="{{csrf_token()}}" data-route="{{route('store.user.associate')}}" action="{{route('store.user.authenticate')}}" method="post">
-                                    @csrf
-                                    <div class="block-content ">
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material">
-                                                    <label for="material-error">Name</label>
-                                                    <input required class="form-control" type="text" id="user-name" name="name"
-                                                           value=""   placeholder="Enter Name">
+                                <ul class="nav nav-tabs nav-tabs-block nav-justified" data-toggle="tabs" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active js-new-user-tab" href="#new">Create a new one</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link js-existing-user-tab" href="#existing">Already have an account?</a>
+                                    </li>
+                                </ul>
 
+                                <div class="block-content tab-content">
+                                    <div class="tab-pane active" role="tabpanel">
+                                        <form id="authenticate_user_form" data-store="{{$shop->shopify_domain}}" data-token="{{csrf_token()}}" data-route="{{route('store.user.associate')}}" action="{{route('store.user.authenticate')}}" method="post">
+                                            @csrf
+                                            <div class="block-content ">
+                                                <div class="form-group">
+                                                    <div class="col-sm-12">
+                                                        <div class="form-material">
+                                                            <label for="material-error">Name</label>
+                                                            <input required class="form-control" type="text" id="user-name" name="name"
+                                                                   value=""   placeholder="Enter Name">
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-sm-12">
+                                                        <div class="form-material">
+                                                            <label for="material-error">Email Address</label>
+                                                            <input required class="form-control" type="email" id="user-email" name="email"
+                                                                   value=""   placeholder="Enter Registered Email Address">
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="col-sm-12">
+                                                        <div class="form-material">
+                                                            <label for="material-error">Password</label>
+                                                            <input required class="form-control" type="password" id="user-password" name="password"
+                                                                   value=""  placeholder="Enter Password">
+
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material">
-                                                    <label for="material-error">Email Address</label>
-                                                    <input required class="form-control" type="email" id="user-email" name="email"
-                                                           value=""   placeholder="Enter Registered Email Address">
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <div class="form-material">
-                                                    <label for="material-error">Password</label>
-                                                    <input required class="form-control" type="password" id="user-password" name="password"
-                                                           value=""  placeholder="Enter Password">
-
-                                                </div>
-                                            </div>
+                                            <input type="submit" hidden>
+                                        </form>
+                                        <div class="block-content block-content-full text-right border-top">
+                                            <button type="submit" class="btn btn-sm authenticate_user btn-primary" >Authenticate</button>
                                         </div>
                                     </div>
-                                    <input type="submit" hidden>
-                                </form>
-                                <div class="block-content block-content-full text-right border-top">
-                                    <button type="submit" class="btn btn-sm authenticate_user btn-primary" >Authenticate</button>
                                 </div>
+
                             </div>
                         </div>
                     </div>
