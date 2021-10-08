0b22b40c7db823ed48c40066bdbef23fee1230a5
Tetralogicx Pvt Ltd <yasir@tetralogicx.com>
2021-10-08 16:22:18 +0500

    Adding Inventory manager code

--- resources/views/non_shopify_users/product/view_product.blade.php
+++ resources/views/non_shopify_users/product/view_product.blade.php
@@ -106,15 +106,15 @@
                                     </div>
                                     <hr>
                                 @endif
-                                @if(count($product->has_sub_sub_categories) > 0)
-                                    <div class="tags" style="margin-top: 5px">
-                                        <h4 style="margin-bottom: 5px">Sub sub categories</h4>
-                                        @foreach($product->has_sub_sub_categories as $sub_sub_cat)
-                                            <span class="badge badge-primary">{{$sub_sub_cat->title}}</span>
-                                        @endforeach
-                                    </div>
-                                    <hr>
-                                @endif
+{{--                                @if(count($product->has_sub_sub_categories) > 0)--}}
+{{--                                    <div class="tags" style="margin-top: 5px">--}}
+{{--                                        <h4 style="margin-bottom: 5px">Sub sub categories</h4>--}}
+{{--                                        @foreach($product->has_sub_sub_categories as $sub_sub_cat)--}}
+{{--                                            <span class="badge badge-primary">{{$sub_sub_cat->title}}</span>--}}
+{{--                                        @endforeach--}}
+{{--                                    </div>--}}
+{{--                                    <hr>--}}
+{{--                                @endif--}}
 
                                 @if($product->tags != null)
                                     <div class="tags" style="margin-top: 5px">
