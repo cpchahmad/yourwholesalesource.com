019705e78090cf08005cbec714cef448586208ff
cpchahmad <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>
2021-12-15 13:55:07 +0500

    aa

--- resources/views/orders/fulfillment.blade.php
+++ resources/views/orders/fulfillment.blade.php
@@ -162,10 +162,9 @@
                     @endphp
                     <div class="block-content">
                         @if($shipping != null)
-                            @php dd($shipping); @endphp
                             <p style="font-size: 14px">{{$shipping->first_name}} {{$shipping->last_name}}
                                 @if($order->custom == 0)
-                                <br> {{$shipping->company}}
+                                @if(isset($shipping->company)) <br> {{$shipping->company}}  @endif
                                 @endif
                                 <br> {{$shipping->address1}}
                                 <br> {{$shipping->address2}}
